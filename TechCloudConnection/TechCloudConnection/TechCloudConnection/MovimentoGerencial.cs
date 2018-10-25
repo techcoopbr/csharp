3adc2bdfa63781d968af38226782aafef666eab0
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-25 18:01:31 -0300

    adição da classe MovimentoGerencial

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MovimentoGerencial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MovimentoGerencial
+    {
+        public int Id { get; set; }
+        public int Idmovimento { get; set; }
+        public string Idplanog { get; set; }
+        public int Idpessoas { get; set; }
+        public DateTime Datalancamento { get; set; }
+        public DateTime Data { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public int Idempresa { get; set; }
+        public string Nomepessoa { get; set; }
+        public int Idacumulador { get; set; }
+        public int Idweb { get; set; }
+    }
+}
