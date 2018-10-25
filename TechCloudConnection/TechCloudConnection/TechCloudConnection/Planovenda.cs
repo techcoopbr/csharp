64815a0a782be3adaacff9c040c61d131fc20c86
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-25 17:52:20 -0300

    adição da classe Planovenda

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/Planovenda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Planovenda
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Parcelas { get; set; }
+        public string Intervalo { get; set; }
+        public int Tipocobranca { get; set; }
+        public string Descricaocobranca { get; set; }
+        public int Relatorio { get; set; }
+        public decimal Parcelaminima { get; set; }
+        public int Ativo { get; set; }
+        public int Diavencimento { get; set; }
+        public int Tododia { get; set; }
+        public int Idboleto { get; set; }
+        public int Empresa { get; set; }
+        public int Imprimecontrato { get; set; }
+        public string Relatoriocontrato { get; set; }
+        public int Imprimeromaneio { get; set; }
+        public string Relatorioromaneio { get; set; }
+        public int Pagacomissao { get; set; }
+        public int Idweb { get; set; }
+}
+}
