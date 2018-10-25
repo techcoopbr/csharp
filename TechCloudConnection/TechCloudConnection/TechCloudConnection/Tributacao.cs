d1846fb4557f3b4fe2917e50d7b33b7311ea2e04
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-25 18:13:59 -0300

    adição da classe Tributacao

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/Tributacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Tributacao
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Cst { get; set; }
+        public decimal Aliquotaref { get; set; }
+        public string Aliquotaecf { get; set; }
+        public string Origem { get; set; }
+        public int Idweb { get; set; }
+    }
+}
