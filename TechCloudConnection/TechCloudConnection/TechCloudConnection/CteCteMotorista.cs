25e83982529f3e50a1c39370291e7192cca1b0f8
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-26 23:15:22 -0300

    Adicionado classe CteCteMotorista

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteMotorista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class CteCteMotorista
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Idmotorista { get; set; }
+        public string Nome { get; set; }
+        public int Empresa { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Vencimento { get; set; }
+        public int Parcelas { get; set; }
+        public int Idweb { get; set; }
+    }
+}
