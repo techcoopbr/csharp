526f6aa2bdd7091bdbc1b7f9209a9f3d12369382
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-26 23:09:22 -0300

    Adicionado classe CteCteFatura

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteFatura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class CteCteFatura
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Parcela { get; set; }
+        public DateTime Vencimento { get; set; }
+        public decimal Valor { get; set; }
+        public int Empresa { get; set; }
+        public int Idweb { get; set; }
+    }
+}
