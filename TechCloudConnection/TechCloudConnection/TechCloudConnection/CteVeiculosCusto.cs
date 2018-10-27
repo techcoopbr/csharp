11f60510414ded2f8bb3cdda0a1ea3ee8b04f766
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-26 23:39:26 -0300

    Adicionado classe CteVeiculosCusto

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteVeiculosCusto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class CteVeiculosCusto
+    {
+        public int Id { get; set; }
+        public int Idveiculo { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public decimal Quantidade { get; set; }
+        public decimal Valorunitario { get; set; }
+        public decimal Valortotal { get; set; }
+        public int Idnf { get; set; }
+        public int Idequipamento { get; set; }
+        public decimal Kmhora { get; set; }
+        public int Idweb { get; set; }
+    }
+}
