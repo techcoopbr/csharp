c2ce91b0f9077aa273a0e63fa90e7e8519ed7c0a
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-26 23:46:15 -0300

    Adicionado classe CteViagem

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteViagem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class CteViagem
+    {
+        public int Codigo { get; set; }
+        public DateTime Saida { get; set; }
+        public DateTime Volta { get; set; }
+        public int Rota { get; set; }
+        public int Idmotorista { get; set; }
+        public decimal Adiantamento { get; set; }
+        public decimal Receitas { get; set; }
+        public decimal Despesas { get; set; }
+        public decimal Outros { get; set; }
+        public bool Aberta { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data_atualizacao { get; set; }
+        public int Empresa { get; set; }
+        public decimal Kminicial { get; set; }
+        public decimal Kmfinal { get; set; }
+        public int Idweb { get; set; }
+    }
+}
