16dc06321429a20e6796a7fc9893c8f848f1e2ce
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-26 22:54:38 -0300

    Adicionado classe CteCteDestend

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteDestend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class CteCteDestend
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public int Idcidade { get; set; }
+        public int Empresa { get; set; }
+        public int Idweb { get; set; }
+    }
+}
