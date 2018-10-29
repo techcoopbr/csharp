f8b73b265c0428260817cfb613c4f299e6998813
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-29 08:57:28 -0300

    Adicionado classe MdfeCarregamento

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfeCarregamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MdfeCarregamento
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public int Idcidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public string Descricao { get; set; }
+        public int Idweb { get; set; }
+    }
+}
