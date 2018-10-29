1e42f460623cf05c36e890fc4af2a2c06120b7c9
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-29 09:07:46 -0300

    Adicionado classe MdfeContratante

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfeContratante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MdfeContratante
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Cnpj { get; set; }
+        public string Nome { get; set; }
+        public int Idweb { get; set; }
+    }
+}
