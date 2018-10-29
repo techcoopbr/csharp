da7631a12a3782b5fa5881af2edc23a26ee27f5e
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-29 09:16:13 -0300

    Adicionado classe MdfePedagios

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfePedagio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MdfePedagio
+    {
+        public int Id { get; set; }
+        public string Idmdfe { get; set; }
+        public string Cnpjpedagio { get; set; }
+        public string Numerocomprovante { get; set; }
+        public string Cnpjpagante { get; set; }
+        public int Idweb { get; set; }
+    }
+}
