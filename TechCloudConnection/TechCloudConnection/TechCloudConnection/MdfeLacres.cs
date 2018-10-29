3076f82598d9b02c0583c3200af11ac26cbeccdf
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-29 09:11:41 -0300

    Adicionado classe MdfeLacres

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfeLacres.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MdfeLacres
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Lacre { get; set; }
+        public int Idweb { get; set; }
+    }
+}
