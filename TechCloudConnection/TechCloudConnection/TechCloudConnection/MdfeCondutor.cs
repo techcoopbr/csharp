a286f95be95643acad510a839e1123a6a2a54252
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-29 09:05:34 -0300

    Adicionado classe MdfeCondutor

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfeCondutor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MdfeCondutor
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Idweb { get; set; }
+    }
+}
