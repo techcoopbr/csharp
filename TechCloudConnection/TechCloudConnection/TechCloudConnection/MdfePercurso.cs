f5b200dd56ef34ca97a3e9b97bc67f13d1ff4144
Darlan Selzlein <sdarlanset@gmail.com>
2018-10-29 09:18:42 -0300

    Adicionado classe MdfePercurso

---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfePercurso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class MdfePercurso
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public int Idweb { get; set; }
+    }
+}
