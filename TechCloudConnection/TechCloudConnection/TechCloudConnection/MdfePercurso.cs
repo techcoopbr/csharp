180df71891b63c649f7b93de6e9bfee603c7579d
Darlan Selzlein <sdarlanset@gmail.com>
2018-11-06 00:34:05 -0200

    Adicionado classe MdfePercursoDAL

--- TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfePercurso.cs
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfePercurso.cs
@@ -9,6 +9,7 @@ namespace Modelo
     class MdfePercurso
     {
         public int Id { get; set; }
+        public int Codigo { get; set; }
         public int Idmdfe { get; set; }
         public string Uf { get; set; }
         public string Ibge { get; set; }
---
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfePercursoDAL.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using FirebirdSql.Data.FirebirdClient;
+using ConexaoDB;
+using Modelo;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+
+namespace Modelo
+{
+    class MdfePercursoDAL
+    {
+        public void Gravar(MdfePercurso per)
+        {
+            if (per.Codigo == 0)
+                Inserir(per);
+            else
+                Atualizar(per);
+        }
+
+        private void Inserir(MdfePercurso per)
+        {
+
+        }
+
+        private void Atualizar(MdfePercurso per)
+        {
+
+        }
+
+        public void Remover(MdfePercurso per)
+        {
+
+        }
+
+
+        public void PostMdfePercurso()
+        {
+            Connection_Query conn = new Connection_Query();
+            conn.OpenConection();
+
+            IniFile NewIniFile = new IniFile("techconfig");
+
+            MdfePercursoDAL pdal = new MdfePercursoDAL();
+
+            FbDataReader dr = conn.DataReader("select p.*, e.idweb from NF p, empresa E where p.sincronizado = 0");
+
+            MdfePercurso p = new MdfePercurso();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+
+                    int DocumentId = 0;
+                    // ENCONTRA O ID ACCOUNT DA EMPRESA DONA DA DUPLICATA
+                    try
+                    {
+                        //string webAddrr = "http://localhost:3000/document_accounts/" + Convert.ToString(dr["codigo"]) + "/" + Convert.ToString(dr["Idweb"]) + ".json";
+
+                        string webAddrr = "http://apptechcoop.com.br/document_accounts/" + Convert.ToString(dr["codigo"]) + "/" + Convert.ToString(dr["Idweb"]) + ".json";
+
+                        var httpWebRequestt = (HttpWebRequest)WebRequest.Create(webAddrr);
+                        httpWebRequestt.ContentType = "application/json; charset=utf-8";
+                        httpWebRequestt.Method = "GET";
+
+                        String usernamee = "admin@jefferson.com";
+                        String passwordd = "2311luje2311";
+                        String encodedd = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("utf-8").GetBytes(usernamee + ":" + passwordd));
+                        httpWebRequestt.Headers.Add("Authorization", "Basic " + encodedd);
+
+
+
+                        var httpResponsee = (HttpWebResponse)httpWebRequestt.GetResponse();
+
+                        using (var streamReaderr = new StreamReader(httpResponsee.GetResponseStream()))
+                        {
+                            var responseTextt = streamReaderr.ReadToEnd();
+                            var serializerr = new JavaScriptSerializer();
+                            dynamic usr = serializerr.DeserializeObject(responseTextt);
+                            DocumentId = usr["id"];
+
+                            conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        DocumentId = 0;
+                    }
+
+                    // ENCONTRA O ID FOLK DA PESSOA DA DUPLICATA
+                    try
+                    {
+                        //string webAddrD = "http://localhost:3000/folk_accounts/" + Convert.ToString(dr["idpessoas"]) + "/" + Convert.ToString(dr["Idweb"]) + ".json";
+
+                        string webAddrD = "http://apptechcoop.com.br/folk_accounts/" + Convert.ToString(dr["idpessoas"]) + "/" + Convert.ToString(dr["Idweb"]) + ".json";
+
+                        var httpWebRequesttD = (HttpWebRequest)WebRequest.Create(webAddrD);
+                        httpWebRequesttD.ContentType = "application/json; charset=utf-8";
+                        httpWebRequesttD.Method = "GET";
+
+                        String usernameeD = "admin@jefferson.com";
+                        String passworddD = "2311luje2311";
+                        String encodeddD = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("utf-8").GetBytes(usernameeD + ":" + passworddD));
+                        httpWebRequesttD.Headers.Add("Authorization", "Basic " + encodeddD);
+
+
+
+                        var httpResponseeD = (HttpWebResponse)httpWebRequesttD.GetResponse();
+
+                        using (var streamReaderrD = new StreamReader(httpResponseeD.GetResponseStream()))
+                        {
+                            var responseTexttD = streamReaderrD.ReadToEnd();
+                            var serializerD = new JavaScriptSerializer();
+                            dynamic usrD = serializerD.DeserializeObject(responseTexttD);
+                            //p.Idfolk = usrD["id"];
+                            // >>>>>>  não existe o campo idfolk na tabela operacoes  <<<<<<
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        //p.Idfolk = 0;
+                    }
+
+                    //////////////////////////////////////////////
+                    try
+                    {
+
+                        string webAddr;
+
+                        if (DocumentId == 0)
+                        {
+                            //webAddr = "http://localhost:3000/documents";
+                            webAddr = "http://apptechcoop.com.br/documents";
+                        }
+                        else
+                        {
+                            //webAddr = "http://localhost:3000/documents/" + Convert.ToString(DocumentId) + ".json";
+                            webAddr = "http://apptechcoop.com.br/documents/" + Convert.ToString(DocumentId) + ".json";
+                        }
+
+                        var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
+                        httpWebRequest.ContentType = "application/json; charset=utf-8";
+                        if (DocumentId == 0)
+                        {
+                            httpWebRequest.Method = "POST";
+                        }
+                        else
+                        {
+                            httpWebRequest.Method = "PUT";
+                        }
+
+                        String username = "admin@jefferson.com";
+                        String password = "2311luje2311";
+                        String encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("utf-8").GetBytes(username + ":" + password));
+                        httpWebRequest.Headers.Add("Authorization", "Basic " + encoded);
+
+                        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                        {
+                            if (Convert.ToInt32(dr["Idweb"]) > 0)
+                            {
+                                if (DocumentId != 0)
+                                {
+                                    p.Codigo = DocumentId;
+                                    NewIniFile.IniWriteString("STATUS", "MSG", "STATUS: ATUALIZANDO DOCUMENTOS...");
+                                }
+                                else
+                                {
+                                    NewIniFile.IniWriteString("STATUS", "MSG", "STATUS: INSERINDO DOCUMENTOS...");
+                                }
+
+                                p.Idweb = (int)Convert.ToInt32(dr["Idweb"]);
+
+                                if (dr["codigo"] != DBNull.Value) { p.Codigo = (int)dr["codigo"]; }
+                                if (dr["idmdfe"] != DBNull.Value) { p.Idmdfe = (int)dr["idmdfe"]; }
+                                if (dr["uf"] != DBNull.Value) { p.Uf = (string)dr["uf"]; }
+                                if (dr["ibge"] != DBNull.Value) { p.Ibge = (string)dr["ibge"]; }
+                                if (dr["account_id"] != DBNull.Value) { p.Idweb = (int)dr["account_id"]; }
+
+
+
+                                //pdal.PostNf(p);
+
+                                string json = "{ ";
+                                if (DocumentId != 0)
+                                {
+                                    json = json + "\"id\" :\"" + DocumentId + "\", ";
+                                }
+                                json = json + "\"codigo\":\"" + p.Codigo + "\"," +
+                                     "\"codigo\":\"" + p.Codigo + "\"," +
+
+                                     "\"idmdfe\":\"" + p.Idmdfe + "\"," +
+                                     "\"uf\":\"" + p.Uf + "\"," +
+                                     "\"ibge\":\"" + p.Ibge + "\"," +
+                                     "\"account_id\":\"" + p.Idweb + "\"}";
+
+                                json = json.Replace("\r\n", "");
+
+                                streamWriter.Write(json);
+                                streamWriter.Flush();
+
+                                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                                {
+                                    var responseText = streamReader.ReadToEnd();
+
+                                }
+
+                                // ENCONTRA O ID ACCOUNT DA EMPRESA DONA DA DUPLICATA
+                                try
+                                {
+                                    //string webAddrr = "http://localhost:3000/document_accounts/" + Convert.ToString(dr["codigo"]) + "/" + Convert.ToString(dr["Idweb"]) + ".json";
+
+                                    string webAddrr = "http://apptechcoop.com.br/document_accounts/" + Convert.ToString(dr["codigo"]) + "/" + Convert.ToString(dr["Idweb"]) + ".json";
+
+                                    var httpWebRequestt = (HttpWebRequest)WebRequest.Create(webAddrr);
+                                    httpWebRequestt.ContentType = "application/json; charset=utf-8";
+                                    httpWebRequestt.Method = "GET";
+
+                                    String usernamee = "admin@jefferson.com";
+                                    String passwordd = "2311luje2311";
+                                    String encodedd = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("utf-8").GetBytes(usernamee + ":" + passwordd));
+                                    httpWebRequestt.Headers.Add("Authorization", "Basic " + encodedd);
+
+
+
+                                    var httpResponsee = (HttpWebResponse)httpWebRequestt.GetResponse();
+
+                                    using (var streamReaderr = new StreamReader(httpResponsee.GetResponseStream()))
+                                    {
+                                        var responseTextt = streamReaderr.ReadToEnd();
+                                        var serializerr = new JavaScriptSerializer();
+                                        dynamic usr = serializerr.DeserializeObject(responseTextt);
+                                        DocumentId = usr["id"];
+
+                                        conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+
+                                    }
+                                }
+                                catch (Exception e)
+                                {
+                                    DocumentId = 0;
+                                }
+
+                                try
+                                {
+                                    MdfePercursoDAL perdal = new MdfePercursoDAL();
+                                    //movdal.PostNfproduto((int)dr["codigo"]);
+
+                                    conn.ExecuteQueries("UPDATE NF P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(p.Codigo));
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                    catch (InvalidCastException e)
+                    {
+                        //conn.ExecuteQueries("UPDATE PESSOAS P SET P.SINCRONIZADO = 0 WHERE P.CODIGO = " + Convert.ToString(p.Codigo));
+                        NewIniFile.IniWriteString("STATUS", "MSG", e.Message);
+                        dr.NextResult();
+                        //conn.CloseConnection();
+                    }
+                }
+            }
+            else
+            {
+                NewIniFile.IniWriteString("STATUS", "MSG", "STATUS: NENHUM REGISTRO NOVO");
+            }
+
+            conn.CloseConnection();
+            NewIniFile.IniWriteString("STATUS", "MSG", "ATUALIZAÇÃO FINALIZADA");
+        }
+
+        public MdfePercurso ObterPorId(long id)
+        {
+            var per = new MdfePercurso();
+
+            return per;
+        }
+    }
+}
