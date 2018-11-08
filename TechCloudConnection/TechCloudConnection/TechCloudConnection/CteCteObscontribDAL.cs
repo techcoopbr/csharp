989f5145f394125467e45c227787370c70082631
Darlan Selzlein <sdarlanset@gmail.com>
2018-11-08 18:48:54 -0200

    Feito Sql CteCteObscontribDAL

--- TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteObscontribDAL.cs
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteObscontribDAL.cs
@@ -46,7 +46,7 @@ namespace Modelo
 
             CteCteObscontribDAL odal = new CteCteObscontribDAL();
 
-            FbDataReader dr = conn.DataReader("select p.*, e.idweb from NF p, empresa E where p.sincronizado = 0");
+            FbDataReader dr = conn.DataReader("select p.*, e.idweb from CTE_CTE_OBSCONTRIB p, empresa E where p.sincronizado = 0");
 
             CteCteObscontrib c = new CteCteObscontrib();
 
@@ -83,7 +83,7 @@ namespace Modelo
                             dynamic usr = serializerr.DeserializeObject(responseTextt);
                             DocumentId = usr["id"];
 
-                            conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                            conn.ExecuteQueries("UPDATE CTE_CTE_OBSCONTRIB P SET P.IDCTECTEOBSONTRIBWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
 
                         }
                     }
@@ -240,7 +240,7 @@ namespace Modelo
                                         dynamic usr = serializerr.DeserializeObject(responseTextt);
                                         DocumentId = usr["id"];
 
-                                        conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                                        conn.ExecuteQueries("UPDATE CTE_CTE_OBSCONTRIB P SET P.IDCTECTEOBSCONTRIBWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
 
                                     }
                                 }
@@ -254,7 +254,7 @@ namespace Modelo
                                     CteCteObscontribDAL obsdal = new CteCteObscontribDAL();
                                     //movdal.PostNfproduto((int)dr["codigo"]);
 
-                                    conn.ExecuteQueries("UPDATE NF P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(c.Codigo));
+                                    conn.ExecuteQueries("UPDATE CTE_CTE_OBSCONTRIB P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(c.Codigo));
                                 }
                                 catch
                                 {
