3654f9c869054855f3032f317740275697e804cc
Darlan Selzlein <sdarlanset@gmail.com>
2018-11-08 18:23:42 -0200

    Feito Sql CteCteFaturaDAL

--- TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteFaturaDAL.cs
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteFaturaDAL.cs
@@ -46,7 +46,7 @@ namespace Modelo
 
             CtecteFaturaDAL fdal = new CtecteFaturaDAL();
 
-            FbDataReader dr = conn.DataReader("select p.*, e.idweb from NF p, empresa E where p.sincronizado = 0");
+            FbDataReader dr = conn.DataReader("select p.*, e.idweb from CTE_CTE_FATURA p, empresa E where p.sincronizado = 0");
 
             CteCteFatura c = new CteCteFatura();
 
@@ -83,7 +83,7 @@ namespace Modelo
                             dynamic usr = serializerr.DeserializeObject(responseTextt);
                             DocumentId = usr["id"];
 
-                            conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                            conn.ExecuteQueries("UPDATE CTE_CTE_FATURA P SET P.IDCTECTEFATURAWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
 
                         }
                     }
@@ -242,7 +242,7 @@ namespace Modelo
                                         dynamic usr = serializerr.DeserializeObject(responseTextt);
                                         DocumentId = usr["id"];
 
-                                        conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                                        conn.ExecuteQueries("UPDATE CTE_CTE_FATURA P SET P.IDCTECTEFATURAWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
 
                                     }
                                 }
@@ -256,7 +256,7 @@ namespace Modelo
                                     CtecteFaturaDAL fatdal = new CtecteFaturaDAL();
                                     //movdal.PostNfproduto((int)dr["codigo"]);
 
-                                    conn.ExecuteQueries("UPDATE NF P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(c.Codigo));
+                                    conn.ExecuteQueries("UPDATE CTE_CTE_FATURA P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(c.Codigo));
                                 }
                                 catch
                                 {
