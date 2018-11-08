555b3331653c3ba231501de2da24e601fc2efdc8
Darlan Selzlein <sdarlanset@gmail.com>
2018-11-08 18:27:59 -0200

    Feito Sql CteCteInstrucaoDAL

--- TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteInstrucaoDAL.cs
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/CteCteInstrucaoDAL.cs
@@ -46,7 +46,7 @@ namespace Modelo
 
             CteCteInstrucaoDAL idal = new CteCteInstrucaoDAL();
 
-            FbDataReader dr = conn.DataReader("select p.*, e.idweb from NF p, empresa E where p.sincronizado = 0");
+            FbDataReader dr = conn.DataReader("select p.*, e.idweb from CTE_CTE_INSTRUCAO p, empresa E where p.sincronizado = 0");
 
             CteCteInstrucao c = new CteCteInstrucao();
 
@@ -83,7 +83,7 @@ namespace Modelo
                             dynamic usr = serializerr.DeserializeObject(responseTextt);
                             DocumentId = usr["id"];
 
-                            conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                            conn.ExecuteQueries("UPDATE CTE_CTE_INSTRUCAO P SET P.IDCTECTEINSTRUCAOWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
 
                         }
                     }
@@ -244,7 +244,7 @@ namespace Modelo
                                         dynamic usr = serializerr.DeserializeObject(responseTextt);
                                         DocumentId = usr["id"];
 
-                                        conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                                        conn.ExecuteQueries("UPDATE CTE_CTE_INSTRUCAO P SET P.IDCTECTEINSTRUCAOWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
 
                                     }
                                 }
@@ -258,7 +258,7 @@ namespace Modelo
                                     CteCteInstrucaoDAL insdal = new CteCteInstrucaoDAL();
                                     //movdal.PostNfproduto((int)dr["codigo"]);
 
-                                    conn.ExecuteQueries("UPDATE NF P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(c.Codigo));
+                                    conn.ExecuteQueries("UPDATE CTE_CTE_INSTRUCAO P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(c.Codigo));
                                 }
                                 catch
                                 {
