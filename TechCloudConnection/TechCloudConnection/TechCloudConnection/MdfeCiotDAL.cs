1552fc20db4acd5fd459f784137cc36c4f6486a6
Darlan Selzlein <sdarlanset@gmail.com>
2018-11-08 23:14:20 -0200

    Feito Sql MdfeCiotDAL

--- TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfeCiotDAL.cs
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/MdfeCiotDAL.cs
@@ -46,7 +46,7 @@ namespace Modelo
 
             MdfeCiotDAL cdal = new MdfeCiotDAL();
 
-            FbDataReader dr = conn.DataReader("select p.*, e.idweb from NF p, empresa E where p.sincronizado = 0");
+            FbDataReader dr = conn.DataReader("select p.*, e.idweb from MDFE_CIOT p, empresa E where p.sincronizado = 0");
 
             MdfeCiot c = new MdfeCiot();
 
@@ -83,7 +83,7 @@ namespace Modelo
                             dynamic usr = serializerr.DeserializeObject(responseTextt);
                             DocumentId = usr["id"];
 
-                            conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                            conn.ExecuteQueries("UPDATE MDFE_CIOT P SET P.IDMDFECIOTWEB = " + Convert.ToString(DocumentId) + " WHERE P.ID = " + Convert.ToString((int)dr["codigo"]));
 
                         }
                     }
@@ -238,7 +238,7 @@ namespace Modelo
                                         dynamic usr = serializerr.DeserializeObject(responseTextt);
                                         DocumentId = usr["id"];
 
-                                        conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                                        conn.ExecuteQueries("UPDATE MDFE_CIOT P SET P.IDMDFECIOTWEB = " + Convert.ToString(DocumentId) + " WHERE P.ID = " + Convert.ToString((int)dr["codigo"]));
 
                                     }
                                 }
@@ -252,7 +252,7 @@ namespace Modelo
                                     MdfeCiotDAL ciodal = new MdfeCiotDAL();
                                     //movdal.PostNfproduto((int)dr["codigo"]);
 
-                                    conn.ExecuteQueries("UPDATE NF P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(c.Codigo));
+                                    conn.ExecuteQueries("UPDATE MDFE_CIOT P SET P.SINCRONIZADO = 1 WHERE P.ID = " + Convert.ToString(c.Codigo));
                                 }
                                 catch
                                 {
