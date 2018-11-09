4ebd1977b35e8fa0f4625299f4455f88e7b110cb
Darlan Selzlein <sdarlanset@gmail.com>
2018-11-08 23:47:40 -0200

    Feito Sql PlanovendaDAL

--- TechCloudConnection/TechCloudConnection/TechCloudConnection/PlanovendaDAL.cs
+++ TechCloudConnection/TechCloudConnection/TechCloudConnection/PlanovendaDAL.cs
@@ -46,7 +46,7 @@ namespace Modelo
 
             PlanovendaDAL pdal = new PlanovendaDAL();
 
-            FbDataReader dr = conn.DataReader("select p.*, e.idweb from NF p, empresa E where p.sincronizado = 0");
+            FbDataReader dr = conn.DataReader("select p.*, e.idweb from PLANOVENDA p, empresa E where p.sincronizado = 0");
 
             Planovenda p = new Planovenda();
 
@@ -83,7 +83,7 @@ namespace Modelo
                             dynamic usr = serializerr.DeserializeObject(responseTextt);
                             DocumentId = usr["id"];
 
-                            conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                            conn.ExecuteQueries("UPDATE PLANOVENDA P SET P.IDPLANOVENDAWEB = " + Convert.ToString(DocumentId) + " WHERE P.ID = " + Convert.ToString((int)dr["codigo"]));
 
                         }
                     }
@@ -266,7 +266,7 @@ namespace Modelo
                                         dynamic usr = serializerr.DeserializeObject(responseTextt);
                                         DocumentId = usr["id"];
 
-                                        conn.ExecuteQueries("UPDATE NF P SET P.IDNFWEB = " + Convert.ToString(DocumentId) + " WHERE P.CODIGO = " + Convert.ToString((int)dr["codigo"]));
+                                        conn.ExecuteQueries("UPDATE PLANOVENDA P SET P.IDPLANOVENDAWEB = " + Convert.ToString(DocumentId) + " WHERE P.ID = " + Convert.ToString((int)dr["codigo"]));
 
                                     }
                                 }
@@ -280,7 +280,7 @@ namespace Modelo
                                     PlanovendaDAL pladal = new PlanovendaDAL();
                                     //movdal.PostNfproduto((int)dr["codigo"]);
 
-                                    conn.ExecuteQueries("UPDATE NF P SET P.SINCRONIZADO = 1 WHERE P.CODIGO = " + Convert.ToString(p.Codigo));
+                                    conn.ExecuteQueries("UPDATE PLANOVENDA P SET P.SINCRONIZADO = 1 WHERE P.ID = " + Convert.ToString(p.Codigo));
                                 }
                                 catch
                                 {
