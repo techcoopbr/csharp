373d09b5b1109c93ae72011ff911501529dd1130
techcoop <suporte@techcoop.com.br>
2018-11-19 20:27:40 +0000

    merge

---
+++ RDP/Simple RDP Client/Simple RDP Client/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Simple_RDP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textConnectionString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axRDPViewer = new AxRDPCOMAPILib.AxRDPViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.axRDPViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textConnectionString
+            // 
+            this.textConnectionString.Location = new System.Drawing.Point(101, 265);
+            this.textConnectionString.Name = "textConnectionString";
+            this.textConnectionString.Size = new System.Drawing.Size(271, 20);
+            this.textConnectionString.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Connection String";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axRDPViewer
+            // 
+            this.axRDPViewer.Enabled = true;
+            this.axRDPViewer.Location = new System.Drawing.Point(12, 12);
+            this.axRDPViewer.Name = "axRDPViewer";
+            this.axRDPViewer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axRDPViewer.OcxState")));
+            this.axRDPViewer.Size = new System.Drawing.Size(360, 232);
+            this.axRDPViewer.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 325);
+            this.Controls.Add(this.axRDPViewer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textConnectionString);
+            this.Name = "Form1";
+            this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.axRDPViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textConnectionString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private AxRDPCOMAPILib.AxRDPViewer axRDPViewer;
+    }
+}
+
---
+++ RDP/TCP to RDP Server/TCP to RDP Converter/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TCP_to_RDP_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textConnectionString = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disconnect\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textConnectionString
+            // 
+            this.textConnectionString.Location = new System.Drawing.Point(12, 18);
+            this.textConnectionString.Name = "textConnectionString";
+            this.textConnectionString.Size = new System.Drawing.Size(320, 20);
+            this.textConnectionString.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 83);
+            this.Controls.Add(this.textConnectionString);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textConnectionString;
+    }
+}
+
