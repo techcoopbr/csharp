afe21413694754f2a8f74cf044a6b141b3463d0a
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:23:12 -0200

    classe Boi_Categoria

---
+++ TechClient/TechClient/Boi_Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Boi_Categoria
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public float Pesom { get; set; }
+        public float Ua { get; set; }
+        public string Sexo { get; set; }
+    }
+}
