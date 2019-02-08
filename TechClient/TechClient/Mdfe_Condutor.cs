d6000f8de8ac6bed9aefb8d225c47d72d85433b3
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:25:40 -0200

    classe Mdfe_Condutor

---
+++ TechClient/TechClient/Mdfe_Condutor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Condutor
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Idmdfecondutorweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
