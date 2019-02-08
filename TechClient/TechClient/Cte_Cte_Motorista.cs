8a78bd80a3c101d1b53cb4214ef81672018f9484
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:41:06 -0200

    classe Cte_Cte_Motorista

---
+++ TechClient/TechClient/Cte_Cte_Motorista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_Motorista
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Idmotorista { get; set; }
+        public string Nome { get; set; }
+        public int Empresa { get; set; }
+        public float Valor { get; set; }
+        public DateTime Vencimento { get; set; }
+        public int Parcelas { get; set; }
+        public int Idctectemotoristaweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
