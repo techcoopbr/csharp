d548edeca015ddfabf3389a290d52f4809511fcc
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:39:10 -0200

    classe Mdfe_Reboque

---
+++ TechClient/TechClient/Mdfe_Reboque.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Reboque
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public int Idveiculo { get; set; }
+        public string Placa { get; set; }
+        public int Tipocarroceria { get; set; }
+        public string Ufveiculo { get; set; }
+        public float Capacidadekg { get; set; }
+        public string Renavam { get; set; }
+        public float Capacidadem3 { get; set; }
+        public float Tara { get; set; }
+        public int Proprietario { get; set; }
+        public int Tipoproprietario { get; set; }
+        public string Cpfcnpj { get; set; }
+        public string Nome { get; set; }
+        public string Ie { get; set; }
+        public string Rntrc { get; set; }
+        public string Uf { get; set; }
+        public int Idmdfereboqueweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
