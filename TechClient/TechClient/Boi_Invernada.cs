cf479035e464968842e0d351a134fdcbad31100c
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:26:27 -0200

    classe Boi_Invernada

---
+++ TechClient/TechClient/Boi_Invernada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Boi_Invernada
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public float Area { get; set; }
+        public int Idpasto { get; set; }
+        public int Idretiro { get; set; }
+    }
+}
