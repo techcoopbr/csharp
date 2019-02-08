dd122419c78274095e4d78c4b3eb544dde8f3f47
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:10:17 -0200

    classe EsCategoriaTrabalhador

---
+++ TechClient/TechClient/Es_CategoriaTrabalhador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Es_CategoriaTrabalhador
+    {
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Grupo { get; set; }
+    }
+}
