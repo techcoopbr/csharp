eee6ccf767c51fdf2dd7c2e5440b72f2355a3f6f
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:26:17 -0200

    classe Cte_Cte_caminhao

---
+++ TechClient/TechClient/Cte_Cte_Caminhao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_Caminhao
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Idcaminhao { get; set; }
+        public string Descricao { get; set; }
+        public int Empresa { get; set; }
+        public int Idctectecaminhaoweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
