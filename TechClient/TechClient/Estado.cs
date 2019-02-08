f121bb493974690f041a8ec0743ce858c6c49d3e
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:06:21 -0200

    classe Estado

---
+++ TechClient/TechClient/Estado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Estado
+    {
+        public string Sigla { get; set; }
+        public string Estado { get; set; }
+        public int Regiaotributaria { get; set; }
+        public string Codigouf { get; set; }
+        public string Ibge { get; set; }
+    }
+}
