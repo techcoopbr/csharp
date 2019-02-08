5acb32255413d06fc0dff8a6cf1184d1ba80bf29
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:07:59 -0200

    classe Comandas

---
+++ TechClient/TechClient/Comandas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comandas
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public string Codigodebarras { get; set; }
+        public int Idpessoa { get; set; }
+        public string Obs { get; set; }
+        public int Pagamentomesa { get; set; }
+    }
+}
