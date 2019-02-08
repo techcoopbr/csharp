a49f420ff397a46e6c03de0bb978b7c85552250a
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 22:46:36 -0200

    AgendaNomes

---
+++ TechClient/TechClient/AgendaNomes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class AgendaNomes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpfcnpj { get; set; }
+        public string Fone1 { get; set; }
+        public string Fone2 { get; set; }
+        public string Fone3 { get; set; }
+        public string Fone4 { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Numero { get; set; }
+        public string Cidadetexto { get; set; }
+        public int Idcidade { get; set; }
+        public char Estado { get; set; }
+        public string Obs1 { get; set; }
+        public string Obs2 { get; set; }
+        public string Obs3 { get; set; }
+        public string Obs4 { get; set; }
+        public string Obs5 { get; set; }
+    }
+}
