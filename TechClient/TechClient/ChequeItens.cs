92c32b3e488bd1eb3acafb90cec961ab3bed8057
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:50:16 -0200

    classe ChequeItens

---
+++ TechClient/TechClient/ChequeItens.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ChequeItens
+    {
+        public int Id { get; set; }
+        public int Idcheque { get; set; }
+        public int Idconta { get; set; }
+        public string Conta { get; set; }
+        public string Historico { get; set; }
+        public float Debito { get; set; }
+        public float Credito { get; set; }
+    }
+}
