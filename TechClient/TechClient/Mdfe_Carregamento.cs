ff3a2ab2c5c70d3fcd6e5360575cf59d7f6434aa
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:17:56 -0200

    classe Mdfe_Carregamento

---
+++ TechClient/TechClient/Mdfe_Carregamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Carregamento
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public int Idcidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public string Descricao { get; set; }
+        public int Idmdfecarregamentoweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
