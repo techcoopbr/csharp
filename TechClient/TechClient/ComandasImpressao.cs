65040c2c3a8b1536a13d7dd1cbbc89bed7ced2bb
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:10:08 -0200

    classe ComandasImpressao

---
+++ TechClient/TechClient/ComandasImpressao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ComandasImpressao
+    {
+        public int Id { get; set; }
+        public int Idcomanda { get; set; }
+        public int Idproduto { get; set; }
+        public int Idgrupo { get; set; }
+        public string Descricao { get; set; }
+        public float Quantidade { get; set; }
+        public string Observacao { get; set; }
+        public DateTime Datahorachegada { get; set; }
+        public DateTime Datahoraenvio { get; set; }
+        public int Finalizado { get; set; }
+        public int Idvendedor { get; set; }
+    }
+}
