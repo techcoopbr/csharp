ec3e2643bd9d1c1994872ce5084b1d0e9b0c9749
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:15:36 -0200

    classe ComandasLog

---
+++ TechClient/TechClient/ComandasLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ComandasLog
+    {
+        public int Id { get; set; }
+        public int Idcomanda { get; set; }
+        public int Idusuario { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Datahora { get; set; }
+        public float Totaldeitens { get; set; }
+        public float Totalvalor { get; set; }
+        public string Motivo { get; set; }
+    }
+}
