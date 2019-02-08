a2324b3d2a6409a21fd65648ed99abf21aefc4db
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:19:58 -0200

    classe Comb_Bicos

---
+++ TechClient/TechClient/Comb_Bicos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Bicos
+    {
+        public int Id { get; set; }
+        public int Idcomb { get; set; }
+        public int Idtanque { get; set; }
+        public float Precoautomacao { get; set; }
+        public string Numserie { get; set; }
+        public string Codigo { get; set; }
+        public string Codautomacao { get; set; }
+        public int Idbomba { get; set; }
+        public float Encerrante { get; set; }
+        public int Incremento { get; set; }
+        public string Status { get; set; }
+        public int Idintauto { get; set; }
+    }
+}
