343d80339d7fdb847ff2900a883d206669b60480
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:44:41 -0200

    classe Cfop

---
+++ TechClient/TechClient/Cfop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cfop
+    {
+        public int Codigo { get; set; }
+        public string cfop { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public string Tipomovi { get; set; }
+        public int Finalidade_nfe { get; set; }
+        public int Pagacomissao { get; set; }
+    }
+}
