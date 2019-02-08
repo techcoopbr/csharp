caecbc089bf0aed5ead55c91624f6b7a6eb8ffb8
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:53:04 -0200

    classe Email_Anexos

---
+++ TechClient/TechClient/Email_Anexos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Email_Anexos
+    {
+        public int Id { get; set; }
+        public int Idemail { get; set; }
+        public string Anexo { get; set; }
+    }
+}
