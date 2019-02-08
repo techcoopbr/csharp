dd9737795a4bac4bf8db6abf18922d45219c5b73
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:40:22 -0200

    classe Apto_Reserva_Tarifas

---
+++ TechClient/TechClient/Apto_Reserva_Tarifas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_Reserva_Tarifas
+    {
+        public int Id { get; set; }
+        public int Idreserva { get; set; }
+        public int Idtarifa { get; set; }
+        public int Dias { get; set; }
+        public float Valorunitario { get; set; }
+        public float Valordesconto { get; set; }
+        public float Valortotal { get; set; }
+        public string Descricao { get; set; }
+        public int Empresa { get; set; }
+    }
+}
