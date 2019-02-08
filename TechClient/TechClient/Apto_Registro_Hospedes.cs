131a818ca2d8654d0b3f61ec55e7ebcebe201980
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:17:04 -0200

    classe Apto_Registro_Hospedes

---
+++ TechClient/TechClient/Apto_Registro_Hospedes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_Registro_Hospedes
+    {
+        public int Id { get; set; }
+        public int Idregistro { get; set; }
+        public int Idpessoa { get; set; }
+        public string Nome { get; set; }
+        public int Empresa { get; set; }
+    }
+}
