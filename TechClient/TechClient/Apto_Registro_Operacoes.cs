971c32253da23b0dc72df42e5a2f7b281e9a6f1b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:22:43 -0200

    classe Apto_Registro_Operacoes

---
+++ TechClient/TechClient/Apto_Registro_Operacoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_Registro_Operacoes
+    {
+        public int Id { get; set; }
+        public int Idregistro { get; set; }
+        public int Idoperacao { get; set; }
+        public float Valortotal { get; set; }
+        public string Descricao { get; set; }
+        public int Tipoop { get; set; }
+        public DateTime Datalancamento { get; set; }
+    }
+}
