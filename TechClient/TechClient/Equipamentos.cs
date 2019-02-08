9a327dcbc354dcafcda42da0c55d86263bab492c
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:03:22 -0200

    classe Equipamentos

---
+++ TechClient/TechClient/Equipamentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Equipamentos
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Disponivel { get; set; }
+        public float Valor { get; set; }
+        public int Empresa { get; set; }
+        public float Oleohoras { get; set; }
+        public string Label2 { get; set; }
+        public string Label3 { get; set; }
+        public string Label4 { get; set; }
+        public DateTime Labeldata1 { get; set; }
+        public DateTime Labeldata2 { get; set; }
+        public int Idproprietario { get; set; }
+        public string Nomedono { get; set; }
+    }
+}
