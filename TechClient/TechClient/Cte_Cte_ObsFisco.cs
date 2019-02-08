2d3c5a348cb7c1be6fa386840134551e66830eea
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:44:38 -0200

    classe Cte_Cte_ObsFisco

---
+++ TechClient/TechClient/Cte_Cte_ObsFisco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_ObsFisco
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Identificador { get; set; }
+        public string Observacao { get; set; }
+        public int Empresa { get; set; }
+        public int Idctecteobsfiscoweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
