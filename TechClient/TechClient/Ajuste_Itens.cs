c2eeffb57cd034bdc7ecf264a686d86eb0d84451
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 22:53:26 -0200

    adicionado classe Ajuste_Itens

---
+++ TechClient/TechClient/Ajuste_Itens.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Ajuste_Itens
+    {
+        public int Id { get; set; }
+        public int Idajuste { get; set; }
+        public int Idproduto { get; set; }
+        public float Descricao { get; set; }
+        public float Estoqueatual { get; set; }
+        public float Estoqueajuste { get; set; }
+        public float Diferenca { get; set; }
+        public int Idtanque { get; set; }
+        public string Md5 { get; set; }
+    }
+}
