8561a2d17a667235d17f031c91b7cdcb3edba003
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:52:17 -0200

    classe Integridade

---
+++ TechClient/TechClient/Integridade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Integridade
+    {
+        public int Id { get; set; }
+        public string Cod { get; set; }
+        public int Codid { get; set; }
+    }
+}
