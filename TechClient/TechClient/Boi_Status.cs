440c85b0fa40a870fc7c9490dcc78c601546d3be
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:32:40 -0200

    classe Boi_Status

---
+++ TechClient/TechClient/Boi_Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Boi_Status
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Era { get; set; }
+    }
+}
