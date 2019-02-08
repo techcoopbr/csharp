13cc6939586612d2660959fd854ba0ea17f0bb74
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:42:54 -0200

    classe Ecf_TotalAliquota

---
+++ TechClient/TechClient/Ecf_TotalAliquota.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Ecf_TotalAliquota
+    {
+        public DateTime Dataecf { get; set; }
+        public string Indice { get; set; }
+        public float Aliquota { get; set; }
+        public string Tipo { get; set; }
+        public string Md5 { get; set; }
+        public int Idecf { get; set; }
+        public int Idmapa { get; set; }
+        public float Valor { get; set; }
+        public int Id { get; set; }
+    }
+}
