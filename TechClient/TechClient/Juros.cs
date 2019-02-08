d098f2ba5378144527c28b5cbb553292a5e8acfa
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:54:56 -0200

    classe Juros

---
+++ TechClient/TechClient/Juros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Juros
+    {
+        public DateTime Datai { get; set; }
+        public DateTime Dataf { get; set; }
+        public float Taxa { get; set; }
+    }
+}
