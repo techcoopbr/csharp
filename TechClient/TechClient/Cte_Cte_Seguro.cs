a49586c9f9cb470a3f8d8960818292112e90e782
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:09:03 -0200

    classe Cte_Cte_Seguro

---
+++ TechClient/TechClient/Cte_Cte_Seguro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_Seguro
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Responsavel { get; set; }
+        public string Seguradora { get; set; }
+        public string Apolice { get; set; }
+        public string Averbacao { get; set; }
+        public float Valor { get; set; }
+        public int Empresa { get; set; }
+        public int Idctecteseguroweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
