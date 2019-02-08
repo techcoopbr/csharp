92db63177fa44eb29e0b3e95f180caa82851e48e
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:19:07 -0200

    classe Barcos

---
+++ TechClient/TechClient/Barcos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Barcos
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Disponivel { get; set; }
+        public float Valor { get; set; }
+        public int Empresa { get; set; }
+    }
+}
