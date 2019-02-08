83e601e47cc91f1b118cfa536a884de80625260e
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:10:26 -0200

    classe Apto_LogTroca

---
+++ TechClient/TechClient/Apto_LogTroca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_LogTroca
+    {
+        public int Id { get; set; }
+        public int Idaptoorigem { get; set; }
+        public int Idaptodestino { get; set; }
+        public int Idpessoa { get; set; }
+        public DateTime Dataentrada { get; set; }
+        public DateTime Datasaida { get; set; }
+        public DateTime Datatroca { get; set; }
+        public DateTime Horatroca { get; set; }
+        public int Idusuario { get; set; }
+    }
+}
