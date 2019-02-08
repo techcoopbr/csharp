9356fe5b64b780ecfafb418a4c83be451c8c05f6
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:01:47 -0200

    classe Cidade

---
+++ TechClient/TechClient/Cidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cidade
+    {
+        public int Codigo { get; set; }
+        public string cidade { get; set; }
+        public string Cep { get; set; }
+        public string Estado { get; set; }
+        public string Ibge { get; set; }
+        public int Paisid { get; set; }
+        public int Codigoanp { get; set; }
+        public string Logo { get; set; }
+    }
+}
