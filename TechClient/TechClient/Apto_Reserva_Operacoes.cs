5dd9604e63aff957464cc361304dc722265fabf9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:38:13 -0200

    classe Apto_Reserva_Operacoes

---
+++ TechClient/TechClient/Apto_Reserva_Operacoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_Reserva_Operacoes
+    {
+        public int Id { get; set; }
+        public int Idreserva { get; set; }
+        public int Idoperacao { get; set; }
+        public float Valortotal { get; set; }
+        public string Descricao { get; set; }
+        public int Tipoop { get; set; }
+        public DateTime Datalancamento { get; set; }
+    }
+}
