e1770f62c6ab3e805f06e18cc9a2d81804e6e370
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:54:41 -0200

    classe Email_Contatos

---
+++ TechClient/TechClient/Email_Contatos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Email_Contatos
+    {
+        public int Id { get; set; }
+        public int Idemail { get; set; }
+        public string Email { get; set; }
+        public int Enviado { get; set; }
+    }
+}
