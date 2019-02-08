d7aa2a14315b8ff725d19531f225dd09f2d4e858
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:01:44 -0200

    classe Apartamentos

---
+++ TechClient/TechClient/Apartamentos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apartamentos
+    {
+        public int Id { get; set; }
+        public string Nroapartamento { get; set; }
+        public int Idtipo { get; set; }
+        public int Idsetor { get; set; }
+        public string Ramal { get; set; }
+        public string Descricao { get; set; }
+        public int Capacidade { get; set; }
+        public int Empresa { get; set; }
+        public int Barco { get; set; }
+    }
+}
