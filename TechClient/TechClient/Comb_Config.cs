c4546a8a6cc12559c0ff8b93c21daa70137d99d9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:35:55 -0200

    classe Comb_Config

---
+++ TechClient/TechClient/Comb_Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Config
+    {
+        public int Id { get; set; }
+        public string Dll { get; set; }
+        public float Descontomaximo { get; set; }
+        public string Portacom { get; set; }
+        public string Modeloautomacao { get; set; }
+        public int Usaautomacao { get; set; }
+        public int Quantxpreco { get; set; }
+        public int Ativaautomacao { get; set; }
+        public int Precobomba { get; set; }
+    }
+}
