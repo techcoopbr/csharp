a5be592d26aa408b81befc6b51388c1815d4c7dd
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:07:20 -0200

    classe ApartamentoTipo_Tarifas

---
+++ TechClient/TechClient/ApartamentoTipo_Tarifas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ApartamentoTipo_Tarifas
+    {
+        public int Id { get; set; }
+        public int Idtipo { get; set; }
+        public float Valor { get; set; }
+        public int Idtarifa { get; set; }
+        public string Descricao { get; set; }
+        public int Empresa { get; set; }
+    }
+}
