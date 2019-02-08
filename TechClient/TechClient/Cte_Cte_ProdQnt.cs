32a87625a029c519722ff40c8efb89bdb1a71f6b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:47:58 -0200

    classe Cte_Cte_ProdQnt

---
+++ TechClient/TechClient/Cte_Cte_ProdQnt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_ProdQnt
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Idunidade { get; set; }
+        public string Unidade { get; set; }
+        public float Quantidade { get; set; }
+        public int Empresa { get; set; }
+        public int Idctecteprodqntweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
