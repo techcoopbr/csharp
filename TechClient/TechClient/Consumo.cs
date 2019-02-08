23b4fc3b07174efc6eb32d15962a8c89c3d8ed5d
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:11:04 -0200

    classe COnsumo

---
+++ TechClient/TechClient/Consumo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Consumo
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public int Idlocal { get; set; }
+        public int Iddepartamento { get; set; }
+        public int Idfuncionario { get; set; }
+        public int Idrequisitante { get; set; }
+        public float Customedio { get; set; }
+        public float Custoreposicao { get; set; }
+        public float Precovenda { get; set; }
+        public int Cancelada { get; set; }
+        public int Idempresa { get; set; }
+        public string Obs { get; set; }
+    }
+}
