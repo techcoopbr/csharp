0c253f80e9f066c945346a4d1ccf4ac97e4ac799
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:39:48 -0200

    classe Comb_Encerrante

---
+++ TechClient/TechClient/Comb_Encerrante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Encerrante
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public float Encerrantei { get; set; }
+        public float Encerrantef { get; set; }
+        public int Idbico { get; set; }
+        public int Idcombustivel { get; set; }
+        public int Idfuncionario { get; set; }
+        public float Quantidade { get; set; }
+        public int Empresa { get; set; }
+        public float Afericoes { get; set; }
+    }
+}
