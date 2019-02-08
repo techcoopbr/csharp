1fd578f3f95081fd3cda96ce9cc7e748e70d62f5
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:51:35 -0200

    classe Email

---
+++ TechClient/TechClient/Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Email
+    {
+        public int Id { get; set; }
+        public string Assunto { get; set; }
+        public DateTime Data { get; set; }
+        public string Status { get; set; }
+        public string Html { get; set; }
+        public DateTime Dataenvio { get; set; }
+        public int Emlote { get; set; }
+        public int Cco { get; set; }
+    }
+}
