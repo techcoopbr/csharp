e07bb7d6196887369d207ed2bb476ca3dc36c62a
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:26:50 -0200

    Classe Departamento

---
+++ TechClient/TechClient/Departamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Departamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Idempresa { get; set; }
+    }
+}
