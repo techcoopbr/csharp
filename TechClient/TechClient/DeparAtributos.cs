181b4846848bb94fbfd84f3466e7aaa7c393b672
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:25:24 -0200

    classe DeparAtributos

---
+++ TechClient/TechClient/DeparAtributos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class DeparAtributos
+    {
+        public string Tipo { get; set; }
+        public string Cstorigem { get; set; }
+        public string Csttroca { get; set; }
+        public float Aliquota { get; set; }
+    }
+}
