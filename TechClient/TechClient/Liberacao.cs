3e87afcf56864f82d78a926ceff054b27f9578d8
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:56:53 -0200

    classe liberacao

---
+++ TechClient/TechClient/Liberacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Liberacao
+    {
+        public string Codigoinicial { get; set; }
+        public string Codigoliberacao { get; set; }
+        public string Nomepc { get; set; }
+        public DateTime Dataliberacao { get; set; }
+        public string Cnpj { get; set; }
+    }
+}
