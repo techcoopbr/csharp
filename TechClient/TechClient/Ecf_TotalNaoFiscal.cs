dac60ffb485a1f3927685b9b58931a01afe64cf7
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:48:29 -0200

    classe Ecf_TotalNaoFiscal

---
+++ TechClient/TechClient/Ecf_TotalNaoFiscal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Ecf_TotalNaoFiscal
+    {
+        public int Idecf { get; set; }
+        public DateTime Dataecf { get; set; }
+        public string Cnf { get; set; }
+        public string Descricao { get; set; }
+        public float Valor { get; set; }
+        public string Md5 { get; set; }
+        public int Idmapa { get; set; }
+        public int Id { get; set; }
+    }
+}
