8daf59a816c69a4aa007378e021958271d3da1a5
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:31:14 -0200

    classe Mdfe_Lacres

---
+++ TechClient/TechClient/Mdfe_Lacres.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Lacres
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Lacre { get; set; }
+        public int Idmdfelacresweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
