a254bbc590727a307094be50db26e5cba56522a4
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:58:27 -0200

    classe ListaPreco

---
+++ TechClient/TechClient/ListaPreco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ListaPreco
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public int Idfuncionario { get; set; }
+        public int Ativo { get; set; }
+    }
+}
