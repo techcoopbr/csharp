228db1e04f5f3e6d0cddca5256c08fad06b3d8d9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:28:29 -0200

    classe Boi_Lancamento

---
+++ TechClient/TechClient/Boi_Lancamento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Boi_Lancamento
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public int Idretiro { get; set; }
+        public int Idcategoria { get; set; }
+        public int Idstatus { get; set; }
+        public int Idinvernada { get; set; }
+        public int Idbasto { get; set; }
+        public int Quantidade { get; set; }
+        public string Tipomovimento { get; set; }
+        public int Idusuario { get; set; }
+        public string Observacao { get; set; }
+        public int Idraca { get; set; }
+    }
+}
