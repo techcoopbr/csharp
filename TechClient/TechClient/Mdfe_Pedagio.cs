02ff6477d2a2c84449bd39de144c88cfba08916f
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:35:32 -0200

    classe Mdfe_Pedagio

---
+++ TechClient/TechClient/Mdfe_Pedagio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Pedagio
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Cnpjpedagio { get; set; }
+        public string Numerocomprovante { get; set; }
+        public string Cnpjpagante { get; set; }
+        public int Idmdfepedagioweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
