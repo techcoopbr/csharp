8b7deec307363855d578d1914c7f8ecb564d1136
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 22:49:07 -0200

    adicionado classe Ajuste]

---
+++ TechClient/TechClient/Ajuste.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Ajuste
+    {
+        public int Id { get; set; }
+        public DateTime Datainicial { get; set; }
+        public DateTime Datafinal { get; set; }
+        public int Idpessoa { get; set; }
+        public DateTime Hora { get; set; }
+        public int Empresa { get; set; }
+        public string Md5 { get; set; }
+    }
+}
