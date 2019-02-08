2b14ade59f1fbb8bc0125974e7decd5a40953df9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:45:57 -0200

    classe Comb_Tanques

---
+++ TechClient/TechClient/Comb_Tanques.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Tanques
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public int Idcombustivel { get; set; }
+        public float Estoque { get; set; }
+        public float Capacidade { get; set; }
+    }
+}
