0e165d0c5ac90430763edcc628f0e3f7890355f2
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:24:35 -0200

    classe Cte_Cte

---
+++ TechClient/TechClient/Cte_Cte.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte
+    {
+        public int Codigo { get; set; }
+        public string Modelo { get; set; }
+        public int Serie { get; set; }
+        public int Documento { get; set; }
+        public DateTime Dataemissao { get; set; }
+        public string Cfop { get; set; }
+        public string Naturezaoperacao { get; set; }
+        public string Chave { get; set; }
+        public string Chaveref { get; set; }
+        public int Tiposervico { get; set; }
+        public int Finalidadeemissao { get; set; }
+        public int Formapagamento { get; set; }
+        public int Cidadeemissao { get; set; }
+        public int Cidadeinicio { get; set; }
+        public int Cidadefim { get; set; }
+        public DateTime Previsaoentrega { get; set; }
+        public int Tomador { get; set; }
+        public int Idtomador { get; set; }
+        public int Idremetente { get; set; }
+        public int Iddestinatario { get; set; }
+        public int Idexpedidor { get; set; }
+        public int Idrecebedor { get; set; }
+        public float Valorcarga { get; set; }
+        public int Produtopredominante { get; set; }
+        public int Outrascaracteristicas { get; set; }
+        public float Valorfrete { get; set; }
+        public float Valorreceber { get; set; }
+        public float Impostosvariavel { get; set; }
+        public string Cst_icms { get; set; }
+        public float Base_icms { get; set; }
+        public float Red_icms { get; set; }
+        public int Aliquota_icms { get; set; }
+        public float Valor_icms { get; set; }
+        public float Credito_icms { get; set; }
+        public string Informacoes_fisco { get; set; }
+        public string Observacoesgerais { get; set; }
+        public int Emissao { get; set; }
+        public int Ambiente { get; set; }
+        public string Status { get; set; }
+        public string Recibo { get; set; }
+        public string Protocolo { get; set; }
+        public string Horarecibo { get; set; }
+        public string Ciot { get; set; }
+        public int Lotacao { get; set; }
+        public string Rntrc { get; set; }
+        public int Tipo_data { get; set; }
+        public int Tipo_hora { get; set; }
+        public DateTime Tipo_datai { get; set; }
+        public DateTime Tipo_dataf { get; set; }
+        public DateTime Tipo_horai { get; set; }
+        public DateTime Tipo_horaf { get; set; }
+        public int Idviagem { get; set; }
+        public DateTime Data_atualizacao { get; set; }
+        public float Pedagio { get; set; }
+        public float Descarga { get; set; }
+        public float Icmsreembolso { get; set; }
+        public int Funcionario { get; set; }
+        public int Empresa { get; set; }
+        public float Acrescimo { get; set; }
+        public int Idoperacao { get; set; }
+        public float Kmincial { get; set; }
+        public float Kmfinal { get; set; }
+        public DateTime Horaemissao { get; set; }
+        public int Idmodelo { get; set; }
+        public int Retira { get; set; }
+        public string Localretirada { get; set; }
+        public string Caracadicional { get; set; }
+        public string Caracservico { get; set; }
+        public string Cidadecoleta { get; set; }
+        public string Cidadeentrega { get; set; }
+        public int Globalizado { get; set; }
+        public int Idctecteweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
