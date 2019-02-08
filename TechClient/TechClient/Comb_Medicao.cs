c2b77848d42a9a98e56ac812688fb04b681307a6
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:44:11 -0200

    classe Comb_Medicao

---
+++ TechClient/TechClient/Comb_Medicao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Medicao
+    {
+        public int Id { get; set; }
+        public int Idtanque { get; set; }
+        public int Idcombustivel { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public float Quantidade { get; set; }
+        public int Empresa { get; set; }
+        public int Idfuncionario { get; set; }
+        public float Quantidadeinicial { get; set; }
+        public float Compras { get; set; }
+        public float Saidas { get; set; }
+    }
+}
