405d0db0d8db71df4ab670ff7dcc13f7cff23edb
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:41:09 -0200

    classe Mdfe_Seguro

---
+++ TechClient/TechClient/Mdfe_Seguro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Seguro
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public int Responsavel { get; set; }
+        public string Cpfcnpj { get; set; }
+        public string Nomeseuradora { get; set; }
+        public string Cnpj { get; set; }
+        public string Apolice { get; set; }
+        public string Averbacao { get; set; }
+        public int Idmdfeseguroweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
