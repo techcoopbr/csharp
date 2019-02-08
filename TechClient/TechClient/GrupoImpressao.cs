32f8a568915a18ca86ae2080b3ad1ae2e20ea3d9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:50:31 -0200

    classe GrupoImpressao

---
+++ TechClient/TechClient/GrupoImpressao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class GrupoImpressao
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Impressora { get; set; }
+        public int Copias { get; set; }
+    }
+}
