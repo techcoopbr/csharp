ea576471a3e3f83d55295957ea514ded3d49dce2
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 22:40:14 -0200

    classe Agenda

---
+++ TechClient/TechClient/Agenda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Agenda
+    {
+        public int Id { get; set; }
+        public int Empresa { get; set; }
+        public string Descricao { get; set; }
+        public string Descricaolonga { get; set; }
+        public DateTime Datainicio { get; set; }
+        public DateTime Datafim { get; set; }
+        public int Diasaviso { get; set; }
+        public int Diasalerta { get; set; }
+        public float Kmtrocafeita { get; set; }
+        public int Idveiculo { get; set; }
+        public int Idpessoa { get; set; }
+        public int Idfuncionario { get; set; }
+        public int Idequipamento { get; set; }
+        public int Tipoagenda { get; set; }
+        public DateTime Horainicio { get; set; }
+        public DateTime Horafim { get; set; }
+        public float Kmtrocaanterior { get; set; }
+        public DateTime Dataexecutado { get; set; }
+    }
+}
