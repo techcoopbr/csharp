7e712ec0f842a6199d7a425dd2207a5770e05468
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:36:07 -0200

    classe BoletoRemessas

---
+++ TechClient/TechClient/BoletoRemessas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class BoletoRemessas
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public int Quantidade { get; set; }
+        public int Idcarteiraboleto { get; set; }
+    }
+}
