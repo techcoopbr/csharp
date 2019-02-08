9394b1c4a2639216919ff15d16392ed108152e8f
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:01:24 -0200

    classe EmpresaSocios

---
+++ TechClient/TechClient/EmpresaSocios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class EmpresaSocios
+    {
+        public int Id { get; set; }
+        public int Idempresa { get; set; }
+        public int Idpessoa { get; set; }
+        public string Nome { get; set; }
+        public int Respondereceita { get; set; }
+        public float Percentual { get; set; }
+        public float Cota { get; set; }
+    }
+}
