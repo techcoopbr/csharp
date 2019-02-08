ef0c6e51a57ff6cd2502ff1edd8dfd5b4f7eb65b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:29:45 -0200

    classe Devolucao

---
+++ TechClient/TechClient/Devolucao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Devolucao
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Idnfproduto { get; set; }
+        public int Idproduto { get; set; }
+        public DateTime Datadevolucao { get; set; }
+        public float Quantidade { get; set; }
+        public float Quantidadesaldo { get; set; }
+        public int Idnforigem { get; set; }
+        public int Idnfprodutoorigem { get; set; }
+    }
+}
