ca1e77d917a597f6aefe3f1eaad520cb42554c67
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:46:34 -0200

    classe Etiquetas_Itens

---
+++ TechClient/TechClient/Etiquetas_Itens.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Etiquetas_Itens
+    {
+        public int Id { get; set; }
+        public int Idetiqueta { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
