58cbcb9dbadbf55027820bf789d9ee95e945e51c
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:25:08 -0200

    classe Apto_Registro_Tarifas

---
+++ TechClient/TechClient/Apto_Registro_Tarifas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_Registro_Tarifas
+    {
+        public int Id { get; set; }
+        public int Idregistro { get; set; }
+        public int Idtipo { get; set; }
+        public int Dias { get; set; }
+        public float Valorunitario { get; set; }
+        public float Valordesconto { get; set; }
+        public float Valortotal { get; set; }
+        public string Descricao { get; set; }
+        public int Empresa { get; set; }
+    }
+}
