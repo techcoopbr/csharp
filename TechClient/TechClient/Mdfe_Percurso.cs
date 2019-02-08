5640bcb02c8e30a88232d15f29fb8633b3223d8c
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:36:54 -0200

    classe Mdfe_Percurso

---
+++ TechClient/TechClient/Mdfe_Percurso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Percurso
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public int Idmdfepercursoweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
