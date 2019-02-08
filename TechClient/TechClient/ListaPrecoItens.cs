1922366b6fa83e9e3aab57d6839457eb1ccbb896
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:00:01 -0200

    classe ListaPrecoItens

---
+++ TechClient/TechClient/ListaPrecoItens.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ListaPrecoItens
+    {
+        public int Id { get; set; }
+        public int Idlistapreco { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public float Valor { get; set; }
+        public float Percentual { get; set; }
+    }
+}
