a84bfffc94de4d4295ca63cede6a0c6f65d3cc52
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:03:53 -0200

    classe Config_Cadastros

---
+++ TechClient/TechClient/Config_Cadastros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Config_Cadastros
+    {
+        public int Idcadastro { get; set; }
+        public string Stringcadastro { get; set; }
+        public string Descricao { get; set; }
+        public string Modulo { get; set; }
+    }
+}
