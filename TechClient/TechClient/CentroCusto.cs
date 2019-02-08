b0bcb2b9132ba1a8818863d3f0ac8e4f7bb1efc5
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:42:54 -0200

    classe CentroCusto

---
+++ TechClient/TechClient/CentroCusto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class CentroCusto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
