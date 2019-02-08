c2b51f61112b4ff06ea17c41ca75efcaaa40bb5a
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:37:36 -0200

    classe Cte_Cte_Fatura

---
+++ TechClient/TechClient/Cte_Cte_Fatura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_Fatura
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public int Parcela { get; set; }
+        public DateTime Vencimento { get; set; }
+        public float Valor { get; set; }
+        public int Empresa { get; set; }
+        public int Idctectefaturaweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
