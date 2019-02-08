636bd3f6eb73d8091e632e61d598df015be6d3c5
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:19:37 -0200

    classe Cte_Veiculos_Itens

---
+++ TechClient/TechClient/Cte_Veiculos_Itens.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Veiculos_Itens
+    {
+        public int Codigo { get; set; }
+        public int Idveiculo { get; set; }
+        public string Descricao { get; set; }
+        public int Idveiculomestre { get; set; }
+        public DateTime Data_atualizacao { get; set; }
+        public int Empresa { get; set; }
+        public string Tipoitem { get; set; }
+        public int Idcteveiculositensweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
