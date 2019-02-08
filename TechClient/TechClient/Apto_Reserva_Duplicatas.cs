5ee2109b0fb4d072662c9d3ba9982aeb3a965200
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:30:19 -0200

    classe Apto_Reserva_Duplicatas

---
+++ TechClient/TechClient/Apto_Reserva_Duplicatas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_Reserva_Duplicatas
+    {
+        public int Id { get; set; }
+        public int Idreserva { get; set; }
+        public int Parcela { get; set; }
+        public DateTime Vencimento { get; set; }
+        public float Valor { get; set; }
+    }
+}
