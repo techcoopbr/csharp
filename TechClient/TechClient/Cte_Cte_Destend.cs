72f5efcb21e82e1b79773d8b85f60340b776238a
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:29:18 -0200

    classe Cte_Cte_Destend

---
+++ TechClient/TechClient/Cte_Cte_Destend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_Destend
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public int Idcidade { get; set; }
+        public int Empresa { get; set; }
+        public int Idctectedestendweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
