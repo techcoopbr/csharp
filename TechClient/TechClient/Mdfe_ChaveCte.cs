87faec20fa738181b3e59662e2562e0d400ddbf1
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:20:05 -0200

    classe Mdfe_ChaveCte

---
+++ TechClient/TechClient/Mdfe_ChaveCte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_ChaveCte
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Chavecte { get; set; }
+        public int Idcidade { get; set; }
+        public int Idmdfechavecteweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
