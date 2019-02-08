8a0edc926f0abb27f6346e38cbc68a7f2fc982ae
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:17:38 -0200

    classe Cte_Veiculos_Custo

---
+++ TechClient/TechClient/Cte_Veiculos_Custos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Veiculos_Custos
+    {
+        public int Id { get; set; }
+        public int Idveiculo { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public float Quantidade { get; set; }
+        public float Valorunitario { get; set; }
+        public float Valortotal { get; set; }
+        public int Idnf { get; set; }
+        public int Idequipamento { get; set; }
+        public float Kmhora { get; set; }
+        public int Idcteveiculoscustosweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
