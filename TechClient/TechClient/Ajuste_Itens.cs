2faad74783b4ff665a446113ffe38db49f89a449
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 22:57:16 -0200

    adicionado classe AlertaRevisao e feito correção Ajuste_Itens

--- TechClient/TechClient/Ajuste_Itens.cs
+++ TechClient/TechClient/Ajuste_Itens.cs
@@ -10,7 +10,7 @@ namespace TechClient
         public int Id { get; set; }
         public int Idajuste { get; set; }
         public int Idproduto { get; set; }
-        public float Descricao { get; set; }
+        public string Descricao { get; set; }
         public float Estoqueatual { get; set; }
         public float Estoqueajuste { get; set; }
         public float Diferenca { get; set; }
---
+++ TechClient/TechClient/AlertaRevisao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class AlertaRevisao
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public DateTime Dataaviso { get; set; }
+        public string Observacao { get; set; }
+    }
+}
