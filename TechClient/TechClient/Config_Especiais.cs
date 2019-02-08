025340b2ceaa76fdeadf03f3cbfe0facbf980160
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:09:22 -0200

    classe Config_Especiais

---
+++ TechClient/TechClient/Config_Especiais.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Config_Especiais
+    {
+        public int Idespeciais { get; set; }
+        public string Modulo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
