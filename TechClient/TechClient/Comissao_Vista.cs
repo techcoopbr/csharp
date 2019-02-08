789d4caf6f92c84c3b75eb7f35160da6d65f666d
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:53:58 -0200

    classe Comissao_Vista

---
+++ TechClient/TechClient/Comissao_Vista.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comissao_Vista
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Idvendedor { get; set; }
+        public float Basecomissao { get; set; }
+        public float Percentual { get; set; }
+        public float Comissao { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
