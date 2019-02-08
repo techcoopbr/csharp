f54836cd349b05121fd97b3bc1e0d0bffe77c9df
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:04:58 -0200

    classe Espera

---
+++ TechClient/TechClient/Espera.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Espera
+    {
+        public int Idproduto { get; set; }
+        public string Tipo { get; set; }
+        public string Descricao { get; set; }
+        public float Valorunitario { get; set; }
+        public float Quantidade { get; set; }
+        public float Valortotal { get; set; }
+        public int Idbico { get; set; }
+        public int Idfuncionario { get; set; }
+        public float Encerrantei { get; set; }
+        public float Encerrantef { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public int Idcaixa { get; set; }
+        public int Idordem { get; set; }
+    }
+}
