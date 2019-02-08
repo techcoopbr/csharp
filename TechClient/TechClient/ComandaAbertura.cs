c5d759e0f3fe8f26f3bde128f793aea8c543eb61
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:06:23 -0200

    classe ComandaAbertura

---
+++ TechClient/TechClient/ComandaAbertura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ComandaAbertura
+    {
+        public int Id { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public float Valor { get; set; }
+    }
+}
