ea8345f303370340108b6b33f0216113e2273ac3
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:24:09 -0200

    classe Mdfe_Ciot

---
+++ TechClient/TechClient/Mdfe_Ciot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Ciot
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public int Numero { get; set; }
+        public string Cpfcnpj { get; set; }
+        public int Idmdfeciotweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
