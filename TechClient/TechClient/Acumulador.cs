11340c8d737cfb89648ab6af2fec49e9dbf0748c
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 22:33:04 -0200

    primeira classe Acumulador

---
+++ TechClient/TechClient/Acumulador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Acumulador
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string acumulador { get; set; }
+    }
+}
