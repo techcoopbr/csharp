52bedbc0c9bda4873c02bf79de589307305f01af
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:37:27 -0200

    classe Comb_Efericao

---
+++ TechClient/TechClient/Comb_Efericao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Efericao
+    {
+        public int Id { get; set; }
+        public int Idbico { get; set; }
+        public int Idcombustivel { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public float Quantidade { get; set; }
+        public int Idcaixa { get; set; }
+        public int Empresa { get; set; }
+        public float Encerrantei { get; set; }
+        public float Encerrantef { get; set; }
+        public int Ecf { get; set; }
+        public int Idfuncionario { get; set; }
+    }
+}
