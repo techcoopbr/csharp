16b77cfd5780979f01c1af89cf9db4fafed46a1f
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 19:11:53 -0200

    classe Es_NaturezaRubrica

---
+++ TechClient/TechClient/Es_NaturezaRubrica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Es_NaturezaRubrica
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Nome { get; set; }
+    }
+}
