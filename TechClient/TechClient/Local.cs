fbbad896b76e2084bba86e78c423e6e525eebfca
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:01:46 -0200

    classe Local

---
+++ TechClient/TechClient/Local.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Local
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Idempresa { get; set; }
+        public int Idreponsavel { get; set; }
+    }
+}
