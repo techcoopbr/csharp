b2b03bf05019986cfb93b508f9d25a45a4e4f8c1
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:27:27 -0200

    classe Mdfe_Contratante

---
+++ TechClient/TechClient/Mdfe_Contratante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Mdfe_Contratante
+    {
+        public int Id { get; set; }
+        public int Idmdfe { get; set; }
+        public string Cnpj { get; set; }
+        public string Nome { get; set; }
+        public int Idmdfecontratanteweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
