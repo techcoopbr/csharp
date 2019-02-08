b21f9c04e64350215355d79afd32b35970809b5e
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-07 23:33:09 -0200

    classe Apto_Reserva_Hospedes

---
+++ TechClient/TechClient/Apto_Reserva_Hospedes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Apto_Reserva_Hospedes
+    {
+        public int Id { get; set; }
+        public int Idreserva { get; set; }
+        public int Idpessoa { get; set; }
+        public string Nome { get; set; }
+        public int Idempresa { get; set; }
+    }
+}
