2259d6bb3e46b98fee5fcd2485050d296c5b9dc8
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:45:00 -0200

    classe Ecf_TotalForma

---
+++ TechClient/TechClient/Ecf_TotalForma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Ecf_TotalForma
+    {
+        public int Idecf { get; set; }
+        public string Codforma { get; set; }
+        public string Descriforma { get; set; }
+        public float Valorforma { get; set; }
+        public string Md5 { get; set; }
+        public DateTime Dataecf { get; set; }
+        public int Idmapa { get; set; }
+        public int Id { get; set; }
+    }
+}
