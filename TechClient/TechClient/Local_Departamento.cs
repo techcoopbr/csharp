382bf01f0099b047ae43c35cdf6f2a335cbf51d0
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:03:23 -0200

    classe Local_Departamento

---
+++ TechClient/TechClient/Local_Departamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Local_Departamento
+    {
+        public int Id { get; set; }
+        public int Idlocal { get; set; }
+        public int Iddepartamento { get; set; }
+        public string Descricao { get; set; }
+        public int Idresponsavel { get; set; }
+    }
+}
