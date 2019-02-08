0f513dd3bd72457c14fa684bbd27a85dc6158a27
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:46:19 -0200

    classe Cte_Cte_Prestacao

---
+++ TechClient/TechClient/Cte_Cte_Prestacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_Prestacao
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public string Nome { get; set; }
+        public float Valor { get; set; }
+        public int Empresa { get; set; }
+        public int Idctecteprestacaoweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
