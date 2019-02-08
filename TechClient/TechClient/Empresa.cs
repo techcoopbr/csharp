ff77747e5bd12b6a2bb90a1b70e094358f496d44
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:59:43 -0200

    classe Empresa

---
+++ TechClient/TechClient/Empresa.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Empresa
+    {
+        public int Codigo { get; set; }
+        public string Razaosocial { get; set; }
+        public string Fantasia { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public int Cidade { get; set; }
+        public string Cxpostal { get; set; }
+        public int Numrua { get; set; }
+        public string Cnpj { get; set; }
+        public string Inscricaoestadual { get; set; }
+        public string Inscricaomunicipal { get; set; }
+        public string Fone { get; set; }
+        public string Fax { get; set; }
+        public string Creditoicms { get; set; }
+        public string Tipoapuracaolucro { get; set; }
+        public string Registrojunta { get; set; }
+        public string Codigoatividade { get; set; }
+        public string Codigonaturezajuridica { get; set; }
+        public string Cep { get; set; }
+        public float Aliquotairrf { get; set; }
+        public float Aliquotapis { get; set; }
+        public float Aliquotacofins { get; set; }
+        public float Aliquotacsl { get; set; }
+        public string Email { get; set; }
+        public string Cst_pis { get; set; }
+        public string Cst_cofins { get; set; }
+        public float Percicmssimples { get; set; }
+        public int Suframa { get; set; }
+        public string Cte_rntrc { get; set; }
+        public DateTime Data_atualizacao { get; set; }
+        public string Email_host { get; set; }
+        public string Email_email { get; set; }
+        public string Email_porta { get; set; }
+        public string Email_senha { get; set; }
+        public string Email_login { get; set; }
+        public string Email_assunto { get; set; }
+        public string Email_mensagem { get; set; }
+        public string Iest { get; set; }
+        public string Im { get; set; }
+        public string Cnae { get; set; }
+        public int Tributacaofederal { get; set; }
+        public int Industria { get; set; }
+        public string Cont_nome { get; set; }
+        public string Cont_cpf { get; set; }
+        public string Cont_cnpj { get; set; }
+        public string Cont_cep { get; set; }
+        public string Cont_endereco { get; set; }
+        public string Cont_numero { get; set; }
+        public string Cont_complemento { get; set; }
+        public string Cont_bairro { get; set; }
+        public string Cont_crc { get; set; }
+        public string Cont_fone { get; set; }
+        public string Cont_fax { get; set; }
+        public string Cont_email { get; set; }
+        public string Cont_ibge { get; set; }
+        public string Complemento { get; set; }
+        public string Nre { get; set; }
+        public string Logo { get; set; }
+        public float Aliquotasimples { get; set; }
+        public int Email_usassl { get; set; }
+        public int Email_usatsl { get; set; }
+        public int Sincronizado { get; set; }
+        public int Idweb { get; set; }
+        public string Tabelapcrfb { get; set; }
+        public int Es_fpas { get; set; }
+        public int Es_codigoterceiro { get; set; }
+        public float Es_aliquotaterceiro { get; set; }
+        public int Es_tipoempresa { get; set; }
+        public string Es_naturezajuridica { get; set; }
+        public int Es_desoneracao { get; set; }
+        public int Es_regimeeletronico { get; set; }
+        public string Es_numerotrabalhadortemporario { get; set; }
+        public bool Es_situacaopj { get; set; }
+        public bool Es_situacaopf { get; set; }
+        public string Contatonome { get; set; }
+        public string Contatocpf { get; set; }
+        public string Contatofone01 { get; set; }
+        public string Contatofone02 { get; set; }
+        public string Contatoemail { get; set; }
+    }
+}
