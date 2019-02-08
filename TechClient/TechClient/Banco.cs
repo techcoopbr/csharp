7a47c7f2e4cbf3586f8c76bea9ea4d267d76cc32
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:14:00 -0200

    classe Banco

---
+++ TechClient/TechClient/Banco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Banco
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Agencia { get; set; }
+        public string Conta { get; set; }
+        public float Limite { get; set; }
+        public int Empresa { get; set; }
+        public string Agenciadv { get; set; }
+        public string Contadv { get; set; }
+    }
+}
