ab6c88fd7d97abe36691a7d2d5bea3696a12c473
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:38:58 -0200

    classe Cte_Cte_Lacres

---
+++ TechClient/TechClient/Cte_Cte_Lacres.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cte_Lacres
+    {
+        public int Codigo { get; set; }
+        public int Idcte { get; set; }
+        public string Lacre { get; set; }
+        public int Empresa { get; set; }
+        public int Idctectelacresweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
