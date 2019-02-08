d319f8be6de638e6d5fcc3ed820e5e53487e6a3b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:46:00 -0200

    classe Cfop_Operacoes

---
+++ TechClient/TechClient/Cfop_Operacoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cfop_Operacoes
+    {
+        public int Id { get; set; }
+        public int Idoperacao { get; set; }
+        public string Descricao { get; set; }
+        public int Idcfop { get; set; }
+        public int Empresa { get; set; }
+    }
+}
