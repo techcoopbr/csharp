1622af7b71f6e734e729983b3105395405814418
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:17:09 -0200

    classe BancosSincronizados

---
+++ TechClient/TechClient/BancosSincronizados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class BancosSincronizados
+    {
+        public string Ip { get; set; }
+        public string Nome { get; set; }
+        public string Caminho { get; set; }
+        public string User { get; set; }
+        public string Senha { get; set; }
+        public int Pessoas { get; set; }
+        public int Produtos { get; set; }
+        public int Id { get; set; }
+        public int Empresa { get; set; }
+    }
+}
