95381b2ccec9c5424703d6e73240e385fd2c0ea1
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:21:50 -0200

    classe Comb_Bomba

---
+++ TechClient/TechClient/Comb_Bomba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Bomba
+    {
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public string Marca { get; set; }
+        public string Descricao { get; set; }
+        public string Serie { get; set; }
+        public string Lacre { get; set; }
+        public int Eletronica { get; set; }
+    }
+}
