3037501fa35582a09e38e1e4e5e2639da13e83ec
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 15:42:14 -0200

    classe Comb_Espera

---
+++ TechClient/TechClient/Comb_Espera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Comb_Espera
+    {
+        public int Id { get; set; }
+        public float Encerrante { get; set; }
+        public int Idbico { get; set; }
+        public float Quantidade { get; set; }
+        public float Valor { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Hora { get; set; }
+        public string Ididentifid { get; set; }
+        public float Valorunitario { get; set; }
+        public string Idstring { get; set; }
+        public float Encerrantef { get; set; }
+    }
+}
