5ddffe4b2e84b0dfbfc3358e29ee40dafe31e613
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 14:24:44 -0200

    classe Boi_Era

---
+++ TechClient/TechClient/Boi_Era.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Boi_Era
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
