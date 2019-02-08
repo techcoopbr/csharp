1c59b9e54d3b76ebeef4a73698af646c584b4664
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 16:16:56 -0200

    classe Cte_Cce

---
+++ TechClient/TechClient/Cte_Cce.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Cte_Cce
+    {
+        public int Id { get; set; }
+        public int Idcte { get; set; }
+        public string Arquivo { get; set; }
+        public int Status { get; set; }
+        public int Sequencia { get; set; }
+        public string Texto { get; set; }
+        public string Chaveevento { get; set; }
+        public DateTime Dataevento { get; set; }
+        public string Grupoalterado { get; set; }
+        public string Nomecampo { get; set; }
+        public int Numeroitem { get; set; }
+        public int Idctecceweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
