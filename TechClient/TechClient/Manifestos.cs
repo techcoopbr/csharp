3688450a2412718281b0a2b4f77ae2b4daa14c04
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 20:07:42 -0200

    classe Manifestos

---
+++ TechClient/TechClient/Manifestos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Manifestos
+    {
+        public string Chave { get; set; }
+        public string Cnpj { get; set; }
+        public string Ie { get; set; }
+        public string Nome { get; set; }
+        public DateTime Datahora { get; set; }
+        public string Tpnf { get; set; }
+        public float Valor { get; set; }
+        public string Protocolo { get; set; }
+        public string Csitnfe { get; set; }
+        public int Nsu { get; set; }
+        public string Arquivo { get; set; }
+        public string Caminho { get; set; }
+        public int Propria { get; set; }
+    }
+}
