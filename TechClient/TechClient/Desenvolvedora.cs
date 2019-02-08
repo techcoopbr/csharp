91d95c6ee20d3888a4fc3a68ae79c5905ef9eb9b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-08 18:28:15 -0200

    classe Desenvolvedora

---
+++ TechClient/TechClient/Desenvolvedora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Desenvolvedora
+    {
+        public string Cnpj { get; set; }
+        public string Ie { get; set; }
+        public string Im { get; set; }
+        public string Razao { get; set; }
+        public string Laudopaf { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Versaopaf { get; set; }
+        public string Arquivos { get; set; }
+        public string Arquivotexto { get; set; }
+        public string Contato { get; set; }
+        public string Email { get; set; }
+        public string Fone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
