19daa085e19dee156e540b2e06c1b6e9810fedae
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:37:38 -0200

    classe Nf_Outros

---
+++ TechClient/TechClient/Nf_Outros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Outros
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Tipoitem { get; set; }
+        public int Codigoitem { get; set; }
+        public string Descricao { get; set; }
+        public float Quantidade { get; set; }
+        public float Valorunitario { get; set; }
+        public float Valortotal { get; set; }
+        public int Empresa { get; set; }
+    }
+}
