19897ccc80664b08ca145758028308b56b9f6ca3
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 17:53:58 -0200

    classe Operacoes_Planog

---
+++ TechClient/TechClient/Operacoes_Planog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Operacoes_Planog
+    {
+        public int Id { get; set; }
+        public int Idoperacao { get; set; }
+        public string Descricao { get; set; }
+        public int Acumulador { get; set; }
+        public float Percentual { get; set; }
+        public string Idplanog { get; set; }
+    }
+}
