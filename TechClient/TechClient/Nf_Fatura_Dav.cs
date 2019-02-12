230ef58a218564bcbc3d8bfb0f878ecc7337670c
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:28:58 -0200

    classe Nf_Fatura_Dav

---
+++ TechClient/TechClient/Nf_Fatura_Dav.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Fatura_Dav
+    {
+        public int Codigo { get; set; }
+        public int Idnf { get; set; }
+        public int Parcela { get; set; }
+        public DateTime Vencimento { get; set; }
+        public float Valor { get; set; }
+        public int Idplanovenda { get; set; }
+    }
+}
