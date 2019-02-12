4f7c105209083c9e811c4a73007d8a1547571ad1
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:15:13 -0200

    classe Nf_Comandas

---
+++ TechClient/TechClient/Nf_Comandas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Comandas
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Idcomanda { get; set; }
+    }
+}
