8332ae2b7852d4fac7bddc604f28daec154aa710
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:35:38 -0200

    classe Nf_Operacoes_Dav

---
+++ TechClient/TechClient/Nf_Operacoes_Dav.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Operacoes_Dav
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Idoperacao { get; set; }
+        public float Valortotal { get; set; }
+        public string Descricao { get; set; }
+        public int Tipoop { get; set; }
+    }
+}
