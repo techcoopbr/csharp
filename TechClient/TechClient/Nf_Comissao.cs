0c8d4cbf39f44e7ccf26ccdc185558729ef00d0f
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:16:59 -0200

    classe Nf_Comissao

---
+++ TechClient/TechClient/Nf_Comissao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Comissao
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public float Basecomissao { get; set; }
+        public float Vendaavista { get; set; }
+        public float Vendaaprazo { get; set; }
+        public int Idvendedor { get; set; }
+        public int Parcelas { get; set; }
+        public float Percavista { get; set; }
+        public float Percaprazo { get; set; }
+    }
+}
