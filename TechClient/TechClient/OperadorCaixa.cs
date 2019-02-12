3372276c322ae2ab42e8b37b8b17ea628c6b5ec8
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 17:57:19 -0200

    classe OperadorCaixa

---
+++ TechClient/TechClient/OperadorCaixa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class OperadorCaixa
+    {
+        public int Id { get; set; }
+        public DateTime Dataabertura { get; set; }
+        public DateTime Horaabertura { get; set; }
+        public DateTime Datafechamento { get; set; }
+        public DateTime Horafechamento { get; set; }
+        public int Idusuario { get; set; }
+        public float Valorentradas { get; set; }
+        public float Valorsaidas { get; set; }
+        public float Trocoinicial { get; set; }
+        public float Trocofinal { get; set; }
+        public int Empresa { get; set; }
+    }
+}
