7f2df4dea5d43e74b7109530524f2bb367b09b46
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:02:54 -0200

    classe Paf_Dav_Log

---
+++ TechClient/TechClient/Paf_Dav_Log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Paf_Dav_Log
+    {
+        public string Num_dav { get; set; }
+        public DateTime Dt_alt { get; set; }
+        public string Cod_item { get; set; }
+        public string Desc_item { get; set; }
+        public float Qtde_item { get; set; }
+        public string Uni_item { get; set; }
+        public float Vl_unit { get; set; }
+        public float Vl_descto { get; set; }
+        public float Vl_acres { get; set; }
+        public float Vl_total { get; set; }
+        public string Sit_trib { get; set; }
+        public float Aliq { get; set; }
+        public string Ind_canc { get; set; }
+        public int Dec_qtde_item { get; set; }
+        public int Dec_vl_unit { get; set; }
+        public string Tip_alt { get; set; }
+        public string Md5 { get; set; }
+        public int Idnf { get; set; }
+        public int Id { get; set; }
+        public DateTime Hora { get; set; }
+    }
+}
