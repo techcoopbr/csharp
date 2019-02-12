be1dc533673957ee8185eb485031c6c53c19fac4
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 17:47:29 -0200

    classe Nsu

---
+++ TechClient/TechClient/Nsu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nsu
+    {
+        public int Ultimonsu { get; set; }
+        public int Cstat { get; set; }
+        public string Xmotivo { get; set; }
+        public DateTime Datahora { get; set; }
+        public int Maximonsu { get; set; }
+    }
+}
