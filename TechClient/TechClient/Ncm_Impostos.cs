e7e4caeb28910a27b98e29cd6725ff40bb518119
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 15:59:30 -0200

    classe Ncm_Impostos

---
+++ TechClient/TechClient/Ncm_Impostos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Ncm_Impostos
+    {
+        public string Ncm { get; set; }
+        public string Ufimposto { get; set; }
+        public float Aliquotanac { get; set; }
+        public float Aliquotainp { get; set; }
+        public float Aliquotamun { get; set; }
+        public float Aliquotaest { get; set; }
+        public DateTime Vigenciainicial { get; set; }
+        public DateTime Vigenciafinal { get; set; }
+        public string Versao { get; set; }
+    }
+}
