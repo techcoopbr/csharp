267c8cede76f0bcab119f1646844f4de53e8c87b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:27:17 -0200

    classe Nf_Fatura

---
+++ TechClient/TechClient/Nf_Fatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Fatura
+    {
+        public int Codigo { get; set; }
+        public int Idnf { get; set; }
+        public int Parcela { get; set; }
+        public DateTime Vencimento { get; set; }
+        public float Valor { get; set; }
+        public int Empresa { get; set; }
+        public float Comissao { get; set; }
+        public float Basecomissao { get; set; }
+        public string Md5 { get; set; }
+        public int Idplanovenda { get; set; }
+    }
+}
