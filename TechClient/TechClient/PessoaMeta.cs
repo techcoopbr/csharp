2fed81884cc36c53fc489930f5b9d84bdb234653
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:16:15 -0200

    classe PessoaMeta

---
+++ TechClient/TechClient/PessoaMeta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class PessoaMeta
+    {
+        public int Id { get; set; }
+        public int Idpessoa { get; set; }
+        public float Valorinicial { get; set; }
+        public float Valorfinal { get; set; }
+        public float Percentual { get; set; }
+    }
+}
