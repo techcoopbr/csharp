a33338a25ebce013a4d0b7c2516789ff84cc73ba
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 17:51:53 -0200

    classe Opercaoes_Multi

---
+++ TechClient/TechClient/Operacoes_Multi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Operacoes_Multi
+    {
+        public int Id { get; set; }
+        public int Idref { get; set; }
+        public int Idoperacao { get; set; }
+        public string Descricao { get; set; }
+        public float Percentualtotal { get; set; }
+    }
+}
