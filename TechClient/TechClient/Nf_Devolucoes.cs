b008a130a601555511c0afa991ac01b4af46f704
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:18:39 -0200

    classe Nf_Devolucoes

---
+++ TechClient/TechClient/Nf_Devolucoes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Devolucoes
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public string Chave { get; set; }
+        public int Documento { get; set; }
+        public string Nre { get; set; }
+        public string Uf { get; set; }
+        public string Aamm { get; set; }
+        public string Cnpjcpf { get; set; }
+        public string Ie { get; set; }
+        public string Modelo { get; set; }
+        public int Serie { get; set; }
+        public string Tipo { get; set; }
+    }
+}
