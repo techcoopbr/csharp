85f5c546ef0697f2abbc57dd66c2896ab6e2634b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:10:13 -0200

    classe Nfe_Instrucao

---
+++ TechClient/TechClient/Nfe_Instrucao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nfe_Instrucao
+    {
+        public int Codigo { get; set; }
+        public int Idnfe { get; set; }
+        public string Cnpj { get; set; }
+        public int Instrucao { get; set; }
+        public int Documento { get; set; }
+        public string Motivo { get; set; }
+        public int Empresa { get; set; }
+    }
+}
