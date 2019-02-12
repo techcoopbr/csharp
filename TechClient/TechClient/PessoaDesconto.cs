9277fddddfbf75983db06fcd210df47d64827f5f
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:12:37 -0200

    classe PessoaDesconto

---
+++ TechClient/TechClient/PessoaDesconto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class PessoaDesconto
+    {
+        public int Id { get; set; }
+        public int Idproduto { get; set; }
+        public float Valor { get; set; }
+        public string Tipopreco { get; set; }
+        public int Idpessoa { get; set; }
+    }
+}
