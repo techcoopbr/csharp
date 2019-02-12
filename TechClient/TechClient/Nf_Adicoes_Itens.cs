c724e6cd9ab72ddc47eb8438b15846fce1c802ca
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:13:52 -0200

    classe Nf_Adicoes_Itens

---
+++ TechClient/TechClient/Nf_Adicoes_Itens.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Adicoes_Itens
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string Codigo { get; set; }
+        public float Desconto { get; set; }
+        public int Numeropedido { get; set; }
+        public int Itempedido { get; set; }
+        public int Idproduto { get; set; }
+        public int Iddi { get; set; }
+        public int Numerodi { get; set; }
+        public int Idnf { get; set; }
+        public int Empresa { get; set; }
+    }
+}
