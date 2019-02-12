2a645fe702f60eb000b800ec6004d24ec5e218c9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:49:12 -0200

    classe Nf_Produto_Controle

---
+++ TechClient/TechClient/Nf_Produto_Controle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Produto_Controle
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Idproduto { get; set; }
+        public string Idlote { get; set; }
+        public DateTime Datafabricacao { get; set; }
+        public DateTime Datalanclote { get; set; }
+        public DateTime Datavencimento { get; set; }
+        public float Quantidade { get; set; }
+        public float Quantidadeporlote { get; set; }
+        public int Idempresa { get; set; }
+    }
+}
