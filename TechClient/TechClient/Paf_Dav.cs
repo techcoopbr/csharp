4a52f6a005eba4d48bf8c876aaa26149c1dd57d6
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:00:30 -0200

    classe Paf_Dav

---
+++ TechClient/TechClient/Paf_Dav.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Paf_Dav
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Numerodav { get; set; }
+        public int Numerodoc { get; set; }
+        public int Idecf { get; set; }
+        public int Coo { get; set; }
+        public string Tipodav { get; set; }
+        public string Status { get; set; }
+        public int Ifnfdocumentofiscal { get; set; }
+        public string Stringdav { get; set; }
+        public string Md5 { get; set; }
+    }
+}
