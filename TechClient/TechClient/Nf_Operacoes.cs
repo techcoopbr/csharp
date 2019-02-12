ed8d174dbd5a9a6a5ad010f08c445f3e3ef96cf2
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:32:39 -0200

    classe Nf_Operacoes

---
+++ TechClient/TechClient/Nf_Operacoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Operacoes
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Idoperacao { get; set; }
+        public float Valortotal { get; set; }
+        public string Descricao { get; set; }
+        public int Tipoop { get; set; }
+        public int Idplanovenda { get; set; }
+        public string Md5 { get; set; }
+        public string Cnpjoperadora { get; set; }
+        public int Bandeira { get; set; }
+        public string Autorizacao { get; set; }
+    }
+}
