dc1eac40e692c62f9770fa88d2129beaf393d244
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:50:40 -0200

    classe Nf_Retirada

---
+++ TechClient/TechClient/Nf_Retirada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Retirada
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public string Cpfcnpj { get; set; }
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public int Idcidade { get; set; }
+        public string Obs { get; set; }
+    }
+}
