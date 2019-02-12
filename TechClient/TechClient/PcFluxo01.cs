a647a08c510e3d3e41a80c4e832b5d41b8cd1fa9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:05:38 -0200

    classe PcFluxo01

---
+++ TechClient/TechClient/PcFluxo01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class PcFluxo01
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
