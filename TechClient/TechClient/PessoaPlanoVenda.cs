fb85ee37abf3dfc098c44288ebc186490fdfd09d
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:18:06 -0200

    classe PessoaPlanoVenda

---
+++ TechClient/TechClient/PessoaPlanoVenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class PessoaPlanoVenda
+    {
+        public int Id { get; set; }
+        public int Idplano { get; set; }
+        public string Descricao { get; set; }
+        public int Idpessoa { get; set; }
+    }
+}
