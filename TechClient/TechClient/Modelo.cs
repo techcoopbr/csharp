abc125918cb3bb6055176b79fd91fdc78328eee9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 15:46:26 -0200

    classe Modelo

---
+++ TechClient/TechClient/Modelo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Modelo
+    {
+        public int Codigo { get; set; }
+        public string modelo { get; set; }
+        public string Descricao { get; set; }
+        public int Doc_producao { get; set; }
+        public int Doc_teste { get; set; }
+        public string Formulario { get; set; }
+        public int Serie { get; set; }
+        public int Servico { get; set; }
+        public int Ativo { get; set; }
+        public int Entrada { get; set; }
+    }
+}
