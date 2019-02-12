dd53a600f793d02fc5c9e87dd71ad84484cf0ac3
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:27:00 -0200

    classe Piloteiros

---
+++ TechClient/TechClient/Piloteiros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Piloteiros
+    {
+        public int Id { get; set; }
+        public int Idpessoa { get; set; }
+        public string Apelido { get; set; }
+        public int Disponivel { get; set; }
+        public float Valor { get; set; }
+        public int Empresa { get; set; }
+    }
+}
