548bec758f50f85d732eb75906e94397968b8093
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 18:14:30 -0200

    classe PessoaHorarios

---
+++ TechClient/TechClient/PessoaHorarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class PessoaHorarios
+    {
+        public int Id { get; set; }
+        public int Idpessoa { get; set; }
+        public DateTime Data { get; set; }
+        public DateTime Dataentrada { get; set; }
+        public DateTime Primeiraentrada { get; set; }
+        public DateTime Primeirasaida { get; set; }
+        public DateTime Segundaentrada { get; set; }
+        public DateTime Segundasaida { get; set; }
+        public DateTime Terceiraentrada { get; set; }
+        public DateTime Terceirasaida { get; set; }
+    }
+}
