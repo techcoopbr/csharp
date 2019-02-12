c355122cf0fc08e0bc60b3d54b9b867760f3d8ea
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:08:20 -0200

    classe Nfe_Cce

---
+++ TechClient/TechClient/Nfe_Cce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nfe_Cce
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public string Arquivo { get; set; }
+        public int Status { get; set; }
+        public int Sequencia { get; set; }
+        public string Texto { get; set; }
+        public string Chaveevento { get; set; }
+        public DateTime Dataevento { get; set; }
+    }
+}
