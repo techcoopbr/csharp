e7681234697dad75832079fa0b7df888583f2717
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:30:45 -0200

    classe Nf_Futura

---
+++ TechClient/TechClient/Nf_Futura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Futura
+    {
+        public int Id { get; set; }
+        public int Idnf { get; set; }
+        public int Idprodutonf { get; set; }
+        public float Quantidadeentrege { get; set; }
+    }
+}
