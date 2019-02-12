472cc5937df0b8932009a5d83ecd10baf004132d
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:47:33 -0200

    classe Nf_ProdutosLog

---
+++ TechClient/TechClient/Nf_ProdutosLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_ProdutosLog
+    {
+        public int Idusuario { get; set; }
+        public DateTime Datahora { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public float Quantidade { get; set; }
+        public int Idnf { get; set; }
+        public string Motivo { get; set; }
+        public int Id { get; set; }
+    }
+}
