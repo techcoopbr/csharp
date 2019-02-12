49f7d3411180d89e3a68d93510ba81af61980729
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:44:24 -0200

    classe Nf_Produtos

---
+++ TechClient/TechClient/Nf_Produtos.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Produtos
+    {
+        public int Codigo { get; set; }
+        public int Idnf { get; set; }
+        public int Codigoproduto { get; set; }
+        public string Cfop { get; set; }
+        public float Quantidade { get; set; }
+        public float Valorunitario { get; set; }
+        public float Valortotal { get; set; }
+        public float Descontos { get; set; }
+        public string Csticms { get; set; }
+        public string Cstpis { get; set; }
+        public string Cstcofins { get; set; }
+        public string Cstipi { get; set; }
+        public float Baseicms { get; set; }
+        public float Baseicmsst { get; set; }
+        public float Basepis { get; set; }
+        public float Basecofins { get; set; }
+        public float Baseipi { get; set; }
+        public float Aliquotaicms { get; set; }
+        public float Aliquotaicmsst { get; set; }
+        public float Aliquotapis { get; set; }
+        public float Aliquotacofins { get; set; }
+        public float Aliquotaipi { get; set; }
+        public float Valoricms { get; set; }
+        public float Valoricmsst { get; set; }
+        public float Valorpis { get; set; }
+        public float Valorcofins { get; set; }
+        public float Valoripi { get; set; }
+        public float Valorcusto { get; set; }
+        public string Descricao { get; set; }
+        public int Tipobcicms { get; set; }
+        public int Tipobcicmsst { get; set; }
+        public float Coringaicms { get; set; }
+        public float Coringaicmsst { get; set; }
+        public float Reducaoicms { get; set; }
+        public float Desconto_rateio { get; set; }
+        public float Seguro { get; set; }
+        public float Outros { get; set; }
+        public float Frete { get; set; }
+        public string Obs { get; set; }
+        public float Baseimportacao { get; set; }
+        public float Valoraduaneiro { get; set; }
+        public float Valorimpostoimportacao { get; set; }
+        public float Valoriof { get; set; }
+        public int Atualizaestoque { get; set; }
+        public string Tiponf { get; set; }
+        public int Cancelada { get; set; }
+        public int Empresa { get; set; }
+        public float Comissao { get; set; }
+        public float Peso { get; set; }
+        public float Impostoaprox { get; set; }
+        public string Unidade { get; set; }
+        public int Idtanque { get; set; }
+        public float Comb_encerrantef { get; set; }
+        public float Valorcustomedio { get; set; }
+        public int Idbico { get; set; }
+        public float Comb_encerrantei { get; set; }
+        public int Idordem { get; set; }
+        public string Ncm { get; set; }
+        public float Impestimado_nac { get; set; }
+        public float Impestimado_imp { get; set; }
+        public float Impestimado_est { get; set; }
+        public float Impestimado_mun { get; set; }
+        public int Entregador { get; set; }
+        public int Requisitante { get; set; }
+        public string Md5 { get; set; }
+        public string Chaveexporta { get; set; }
+        public string Obsitem { get; set; }
+        public int Canceladoitem { get; set; }
+        public float Piscofinssomado { get; set; }
+        public string Cod_produto { get; set; }
+        public DateTime Datainclusao { get; set; }
+        public string Nre { get; set; }
+        public string Cest { get; set; }
+        public string Origem_cst { get; set; }
+        public int Iditemcomanda { get; set; }
+        public float Baseiss { get; set; }
+        public float Valoriss { get; set; }
+        public float Aliquotaiss { get; set; }
+        public float Estoqueanterior { get; set; }
+        public float Estoqueanteriornf { get; set; }
+        public float Saldoanterior { get; set; }
+        public float Saldoposterior { get; set; }
+        public int Idnforigem { get; set; }
+        public int Idnfprodutoorigem { get; set; }
+        public string Unidadetributada { get; set; }
+        public float Quantidadetributavel { get; set; }
+        public float Valorunitariotributavel { get; set; }
+        public float Aliquotaufdestino { get; set; }
+        public float Aliquotafundoprobreza { get; set; }
+        public float Baseufdestino { get; set; }
+        public float Basefundopobreza { get; set; }
+        public string Enquadramentoipi { get; set; }
+        public int Bloq_estoque { get; set; }
+        public int Bloq_desconto { get; set; }
+        public int Bloq_validade { get; set; }
+        public float Valorliquido { get; set; }
+        public int Idnfprodutosweb { get; set; }
+        public int Sincronizado { get; set; }
+        public float Valorunatual { get; set; }
+    }
+}
