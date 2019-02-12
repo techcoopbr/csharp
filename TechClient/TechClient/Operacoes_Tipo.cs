72832e732a193efe3b9f33bc00e69cd6f762e049
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 17:55:44 -0200

    classe Operacoes_Tipo

---
+++ TechClient/TechClient/Operacoes_Tipo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Operacoes_Tipo
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Tipo { get; set; }
+        public int Grupoop { get; set; }
+    }
+}
