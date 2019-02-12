eb8b2cc82ce9e661f3002e3a70b648e5ed5d87e5
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 16:20:25 -0200

    classe Nf_Energia

---
+++ TechClient/TechClient/Nf_Energia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Nf_Energia
+    {
+        public int Codigo { get; set; }
+        public int Idnf { get; set; }
+        public int Classeconsumo { get; set; }
+        public float Baseicms { get; set; }
+        public float Baseicmsst { get; set; }
+        public float Valoricms { get; set; }
+        public float Valoricmsst { get; set; }
+        public float Valorconsumido { get; set; }
+        public float Valorfatura { get; set; }
+        public float Valordesconto { get; set; }
+        public float Valordespesas { get; set; }
+        public float Valornaotributado { get; set; }
+        public float Valorterceiros { get; set; }
+        public float Valorpis { get; set; }
+        public float Valorcofins { get; set; }
+        public int Tipoligacao { get; set; }
+        public int Tipotensao { get; set; }
+        public string Cfop { get; set; }
+        public float Aliquotaicms { get; set; }
+        public int Empresa { get; set; }
+    }
+}
