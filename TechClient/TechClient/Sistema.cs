fdce90c07301a619175988f3991562930e0f7759
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:07:23 -0200

    classe Sistema

---
+++ TechClient/TechClient/Sistema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Sistema
+    {
+        public string Versao { get; set; }
+        public string Versao_cte { get; set; }
+        public string Versao_hospedeiro { get; set; }
+        public string Versao_consulta { get; set; }
+        public string Versao_banco { get; set; }
+        public int Codigo { get; set; }
+        public int Versaocest { get; set; }
+        public int Versaoncm { get; set; }
+        public int Versaoibpt { get; set; }
+        public int Versaotributacao { get; set; }
+    }
+}
