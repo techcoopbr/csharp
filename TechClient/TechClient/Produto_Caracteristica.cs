0a788fe6f628049bce41102a06373000cd92ace9
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:38:30 -0200

    classe Produto_Caracteristica

---
+++ TechClient/TechClient/Produto_Caracteristica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produto_Caracteristica
+    {
+        public int Idproduto { get; set; }
+        public int Idempresa { get; set; }
+        public int Idtributacao { get; set; }
+        public string Locacao { get; set; }
+    }
+}
