4b9510d8d8993e345256d28c0fb0b6f3473ba355
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:00:39 -0200

    classe Romaneio_Itens

---
+++ TechClient/TechClient/Romaneio_Itens.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Romaneio_Itens
+    {
+        public int Id { get; set; }
+        public int Idromaneio { get; set; }
+        public int Idnf { get; set; }
+        public int Iditem { get; set; }
+        public float Quantidade { get; set; }
+        public DateTime Dataentrega { get; set; }
+        public int Idnfitem { get; set; }
+    }
+}
