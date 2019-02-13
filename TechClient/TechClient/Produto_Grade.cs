ff345fc20f6febe599f426f33e35d865d98c8eaa
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:45:37 -0200

    classe Produto_Grade

---
+++ TechClient/TechClient/Produto_Grade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produto_Grade
+    {
+        public int Idproduto { get; set; }
+        public int Idgrade { get; set; }
+        public string Descricao { get; set; }
+        public string D_cor { get; set; }
+        public string D_tamanho { get; set; }
+        public string Observacao { get; set; }
+    }
+}
