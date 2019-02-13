7668a21588f30f02f865aee4d5c8023d1d90d323
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:15:10 -0200

    classe TipoPlanoContaRfb

---
+++ TechClient/TechClient/TipoPlanoContaRfb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class TipoPlanoContaRfb
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
