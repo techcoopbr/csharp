0dc68f2f7ab28f8a8c3dc3be6e1a169bb9032131
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:31:28 -0200

    classe Usuarios_Tabela

---
+++ TechClient/TechClient/Usuarios_Tabela.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Usuarios_Tabela
+    {
+        public int Idusuarios { get; set; }
+        public string Campo { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
