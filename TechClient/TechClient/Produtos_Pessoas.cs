d91fb83c126f80ad4a50c8b5a4f15ed885afbaab
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:36:25 -0200

    classe Produtos_Pessoas

---
+++ TechClient/TechClient/Produtos_Pessoas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produtos_Pessoas
+    {
+        public int Codigoempresa { get; set; }
+        public string Codigofornecedor { get; set; }
+        public string Cnpj { get; set; }
+        public string Codigobarras { get; set; }
+        public float Fator { get; set; }
+    }
+}
