115d05c50f953c831beb41a9cdac9c489af567e8
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:40:43 -0200

    classe Produto_Controle

---
+++ TechClient/TechClient/Produto_Controle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produto_Controle
+    {
+        public int Id { get; set; }
+        public int Idproduto { get; set; }
+        public string Idlote { get; set; }
+        public DateTime Datafabricacao { get; set; }
+        public DateTime Datalote { get; set; }
+        public DateTime Datavencimento { get; set; }
+        public float Quantidadeporlote { get; set; }
+        public float Estoque { get; set; }
+        public DateTime Datafim { get; set; }
+        public int Idempresa { get; set; }
+        public int Status { get; set; }
+    }
+}
