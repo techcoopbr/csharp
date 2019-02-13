5e8daa9c199c49c873a10e03059031ca8bc11cfd
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:15:18 -0200

    classe PlanoCOntaCrfb

---
+++ TechClient/TechClient/PlanoContaCrfb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class PlanoContaCrfb
+    {
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Dt_ini { get; set; }
+        public string Dt_fim { get; set; }
+        public string Tipo { get; set; }
+        public string Contasuperior { get; set; }
+        public int Nivel { get; set; }
+        public int Natureza { get; set; }
+        public string Orientacoes { get; set; }
+        public string Grupo { get; set; }
+    }
+}
