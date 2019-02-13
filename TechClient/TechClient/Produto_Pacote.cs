ec1dade2e6fa15061a5b79acdee0aa2dfe2a4dfc
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:49:08 -0200

    classe Produto_Pacote

---
+++ TechClient/TechClient/Produto_Pacote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produto_Pacote
+    {
+        public int Idproduto { get; set; }
+        public float Quantidade { get; set; }
+        public float Valorunitario { get; set; }
+        public float Valortotal { get; set; }
+        public string Descricao { get; set; }
+        public int Idprodutoproduto { get; set; }
+    }
+}
