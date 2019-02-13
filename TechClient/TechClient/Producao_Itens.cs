d948284a2b617b7b862ff4bcaeea656546457eac
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:29:58 -0200

    classe Producao_Itens

---
+++ TechClient/TechClient/Producao_Itens.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Producao_Itens
+    {
+        public int Id { get; set; }
+        public int Idproducao { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public float Quantidade { get; set; }
+        public float Custoreposicao { get; set; }
+        public float Customedio { get; set; }
+        public float Estoqueanterior { get; set; }
+        public DateTime Datainclusao { get; set; }
+        public int Idfuncionario { get; set; }
+        public float Estoqueanteriornf { get; set; }
+    }
+}
