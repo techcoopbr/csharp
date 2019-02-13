d45e176321f7613151ef2c81f7f9cc87caed9d00
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:53:53 -0200

    classe Recibo

---
+++ TechClient/TechClient/Recibo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Recibo
+    {
+        public int Id { get; set; }
+        public int Idpessoa { get; set; }
+        public DateTime Datalancamento { get; set; }
+        public float Valorrecebido { get; set; }
+        public float Valorsaldo { get; set; }
+        public float Valordesconto { get; set; }
+        public float Valorjuros { get; set; }
+        public float Valortroco { get; set; }
+    }
+}
