6389aa420fcaef73dfcc8a443cfd057af0a3861d
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:26:42 -0200

    classe Producao

---
+++ TechClient/TechClient/Producao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Producao
+    {
+        public int Id { get; set; }
+        public DateTime Dataabertura { get; set; }
+        public DateTime Datafechamento { get; set; }
+        public DateTime Dataprevisao { get; set; }
+        public string Status { get; set; }
+        public int Idfuncionario { get; set; }
+        public DateTime Horaabertura { get; set; }
+        public DateTime Horaprevisao { get; set; }
+        public DateTime Horafechamento { get; set; }
+    }
+}
