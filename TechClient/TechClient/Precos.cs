d4241e0abe306d7b0c955466c6dea9d460d0dcab
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:23:02 -0200

    classe Precos

---
+++ TechClient/TechClient/Precos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Precos
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public int Idpessoa { get; set; }
+        public DateTime Hora { get; set; }
+        public int Idnf { get; set; }
+        public int Idempresa { get; set; }
+    }
+}
