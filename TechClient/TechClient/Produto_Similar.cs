592ef971155b178375dbb0bfad70c074d0764e87
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:50:48 -0200

    classe Produto_Similar

---
+++ TechClient/TechClient/Produto_Similar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produto_Similar
+    {
+        public int P1 { get; set; }
+        public int P2 { get; set; }
+        public string Descricao { get; set; }
+        public string D_tamanho { get; set; }
+        public string D_cor { get; set; }
+        public string Observacao { get; set; }
+    }
+}
