e0f85b69f271eae6912ff243c0bf3ec81b28b77e
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:33:10 -0200

    classe Usuario_Modelos

---
+++ TechClient/TechClient/Usuario_Modelos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Usuario_Modelos
+    {
+        public int Id { get; set; }
+        public int Idusuario { get; set; }
+        public int Idmodelo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
