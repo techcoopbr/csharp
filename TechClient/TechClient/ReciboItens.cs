0ac04803bb5887c42970850ad43be13628ae309b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:55:02 -0200

    classe ReciboItens

---
+++ TechClient/TechClient/ReciboItens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class ReciboItens
+    {
+        public int Id { get; set; }
+        public int Idrecibo { get; set; }
+        public int Idduplicatabaixa { get; set; }
+    }
+}
