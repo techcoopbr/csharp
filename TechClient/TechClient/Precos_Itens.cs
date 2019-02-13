7db8e9bcd8385c3117ab87a82d812c5111880600
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:24:38 -0200

    classe Precos_Itens

---
+++ TechClient/TechClient/Precos_Itens.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Precos_Itens
+    {
+        public int Id { get; set; }
+        public int Idprecos { get; set; }
+        public int Idprodutos { get; set; }
+        public string Descricao { get; set; }
+        public float Customedio { get; set; }
+        public float Custoreposicao { get; set; }
+        public float Margemlucro { get; set; }
+        public float Precovenda { get; set; }
+        public float Precoprazo { get; set; }
+        public float Precoatacado { get; set; }
+        public float Oldmedio { get; set; }
+        public float Oldreposicao { get; set; }
+        public float Oldmargem { get; set; }
+        public float Oldvenda { get; set; }
+        public float Oldprazo { get; set; }
+        public float Oldatacado { get; set; }
+        public float Margemlucro2 { get; set; }
+        public float Oldmargem2 { get; set; }
+    }
+}
