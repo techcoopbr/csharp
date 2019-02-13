1ea2dd3abd30688278cea8b725149c9ab3823e93
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:27:01 -0200

    classe Usuario_Atalhos

---
+++ TechClient/TechClient/Usuarios_Atalhos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Usuarios_Atalhos
+    {
+        public int Id { get; set; }
+        public int Idusuario { get; set; }
+        public int Idatalho { get; set; }
+        public string Descricao { get; set; }
+    }
+}
