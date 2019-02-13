e9b560f9ec47152095ba4612f8d87b03bb34debe
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:28:17 -0200

    classe Producao_Final

---
+++ TechClient/TechClient/Producao_Final.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Producao_Final
+    {
+        public int Id { get; set; }
+        public int Idproducao { get; set; }
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public float Quantidade { get; set; }
+        public float Custo { get; set; }
+        public float Customedio { get; set; }
+        public float Estoqueatual { get; set; }
+        public float Estoqueatualnf { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
