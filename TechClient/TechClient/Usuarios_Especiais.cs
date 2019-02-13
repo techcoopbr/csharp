9de7310566a2100c329b7d1b85ca66897cd59b28
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:30:14 -0200

    classe Usuario_Especias

---
+++ TechClient/TechClient/Usuarios_Especiais.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Usuarios_Especiais
+    {
+        public int Idusuarios { get; set; }
+        public int Idconfigespecial { get; set; }
+        public int Permite { get; set; }
+        public string Descricao { get; set; }
+    }
+}
