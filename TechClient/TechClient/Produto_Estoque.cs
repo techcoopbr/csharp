79b4254710fed637a567dc125ad7e8bc0e6d35d6
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:42:13 -0200

    classe Produto_Estoque

---
+++ TechClient/TechClient/Produto_Estoque.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produto_Estoque
+    {
+        public int Idproduto { get; set; }
+        public float Vendasdav { get; set; }
+        public float Vendaspre { get; set; }
+        public float Vendaspedido { get; set; }
+        public float Vendassnf { get; set; }
+        public int Idempresa { get; set; }
+        public float Vendasreservado { get; set; }
+        public float Vendasos { get; set; }
+    }
+}
