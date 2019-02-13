bdc9a25182aefd4b7df289c1327e38a7a82a8b04
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-12 22:45:59 -0200

    classe PlanoContaC

---
+++ TechClient/TechClient/PlanoContaC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class PlanoContaC
+    {
+        public string Codigo { get; set; }
+        public string Tipo { get; set; }
+        public bool Total { get; set; }
+        public string Descricao { get; set; }
+        public int Incide { get; set; }
+        public string Pcrfb { get; set; }
+        public int Idplanocontacweb { get; set; }
+        public int Sincronizado { get; set; }
+    }
+}
