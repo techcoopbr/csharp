c3e6fa1ad5d3c0ef444e8418bf22460b28aa9b8d
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:28:23 -0200

    classe Usuarios_Cadastro

---
+++ TechClient/TechClient/Usuarios_Cadastro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Usuarios_Cadastro
+    {
+        public int Idusuarios { get; set; }
+        public int Idconfigcad { get; set; }
+        public int Incluir { get; set; }
+        public int Alterar { get; set; }
+        public int Deletar { get; set; }
+        public int Relatorio { get; set; }
+        public int Detalhes { get; set; }
+        public int Outros { get; set; }
+    }
+}
