caab8b64ccf4fce08efe3a146a42b4a8f32fa8be
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:34:37 -0200

    classe Produtos

---
+++ TechClient/TechClient/Produtos.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produtos
+    {
+        public string Codigoproduto { get; set; }
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Unidade { get; set; }
+        public float Customedio { get; set; }
+        public float Custovenda { get; set; }
+        public float Preco { get; set; }
+        public float Estoque { get; set; }
+        public int Ncm { get; set; }
+        public int Tributacao { get; set; }
+        public string Codigobarras { get; set; }
+        public string Tipoproduto { get; set; }
+        public int Lubrificante { get; set; }
+        public string Codigoanp { get; set; }
+        public string Codigogerencial { get; set; }
+        public int Empresa { get; set; }
+        public int Grupo { get; set; }
+        public int Subgrupo { get; set; }
+        public int Marca { get; set; }
+        public string Unsaida { get; set; }
+        public float Fatorconversao { get; set; }
+        public float Precoprazo { get; set; }
+        public float Precoatacado { get; set; }
+        public float Margemlucro { get; set; }
+        public int Produtosecundario { get; set; }
+        public float Fatorsecundario { get; set; }
+        public string Tipagem { get; set; }
+        public float Comissao { get; set; }
+        public int Tributacaosuperior { get; set; }
+        public int Isentoipi { get; set; }
+        public float Peso { get; set; }
+        public string Item { get; set; }
+        public float Estoquef { get; set; }
+        public float Estoqueg { get; set; }
+        public float G_pontocompra { get; set; }
+        public float G_minimo { get; set; }
+        public float G_maximo { get; set; }
+        public string Locacao { get; set; }
+        public string Descricaoalongada { get; set; }
+        public string Aplicacao { get; set; }
+        public float Margem1pp { get; set; }
+        public float Margem2pp { get; set; }
+        public float Margem1pa { get; set; }
+        public float Margem2pa { get; set; }
+        public float Margemlucro2 { get; set; }
+        public int Ativo { get; set; }
+        public int Grade_pai { get; set; }
+        public int Grade_filho { get; set; }
+        public string D_cor { get; set; }
+        public string D_tamanho { get; set; }
+        public string D_largura { get; set; }
+        public string D_altura { get; set; }
+        public string D_profundidade { get; set; }
+        public string D_capacidade { get; set; }
+        public string Md5 { get; set; }
+        public string Enquadramentoipi { get; set; }
+        public int Controlelote { get; set; }
+        public int Pacote { get; set; }
+        public DateTime Data_atualizacao { get; set; }
+        public string Iat { get; set; }
+        public string Ippt { get; set; }
+        public string Stecf { get; set; }
+        public string Cest { get; set; }
+        public float Descontomaximo { get; set; }
+        public string Codigobalanca { get; set; }
+        public int Permitemudarvalor { get; set; }
+        public int Idgrupoimpressao { get; set; }
+        public int Temobscomanda { get; set; }
+        public int Usaprecobalanca { get; set; }
+        public int Validadebalanca { get; set; }
+        public string Codigoservico { get; set; }
+        public int Produtocompermissao { get; set; }
+        public int Sincronizado { get; set; }
+        public int Estoquesecundario { get; set; }
+        public string Untributada { get; set; }
+        public float Fatoruntributada { get; set; }
+        public float Precopacote { get; set; }
+        public float Quantidadepacote { get; set; }
+        public int Idprodutosweb { get; set; }
+        public int Imprimeficha { get; set; }
+        public int Calculaquantidade { get; set; }
+    }
+}
