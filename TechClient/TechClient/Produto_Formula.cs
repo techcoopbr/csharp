4359f5a4eef316c7bcb6e85f48fd4172a748894a
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 00:43:50 -0200

    classe Produto_Formula

---
+++ TechClient/TechClient/Produto_Formula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Produto_Formula
+    {
+        public int Idproduto { get; set; }
+        public string Descricao { get; set; }
+        public float Quantidade { get; set; }
+        public int Idprodutoformula { get; set; }
+    }
+}
