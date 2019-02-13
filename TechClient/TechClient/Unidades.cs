fec261a0f051fb1996d7a6889d5788bed484d009
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:23:36 -0200

    classe Unidades

---
+++ TechClient/TechClient/Unidades.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Unidades
+    {
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public int Permitefatoracao { get; set; }
+        public int Ativo { get; set; }
+    }
+}
