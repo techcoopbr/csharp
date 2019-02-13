c9bfd40c497c29277b76cb0f0cecf2cd3c011971
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:16:39 -0200

    classe Tributacao

---
+++ TechClient/TechClient/Tributacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Tributacao
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Cst { get; set; }
+        public float Aliquotaref { get; set; }
+        public string Aliquotaecf { get; set; }
+        public string Origem { get; set; }
+    }
+}
