9e4b7c069ec5a0648ba32980f98d757db9a7661d
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:05:56 -0200

    classe Setor

---
+++ TechClient/TechClient/Setor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Setor
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Empresa { get; set; }
+    }
+}
