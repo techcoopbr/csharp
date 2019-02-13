12bcc631361a30ba9d10f18e3aa93c011ac3448b
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:01:57 -0200

    classe Saldo

---
+++ TechClient/TechClient/Saldo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Saldo
+    {
+        public int Id { get; set; }
+        public int Idpessoa { get; set; }
+        public int Idoperacao { get; set; }
+        public DateTime Data { get; set; }
+        public int Idfuncionario { get; set; }
+        public int Idempresa { get; set; }
+        public float Valor { get; set; }
+        public float Saldo { get; set; }
+    }
+}
