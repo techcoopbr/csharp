c697e79fb5ffb18aa35bddc23e4d9774aeacfc6f
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:08:30 -0200

    classe Sped_Generos

---
+++ TechClient/TechClient/Sped_Generos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Sped_Generos
+    {
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Data { get; set; }
+    }
+}
