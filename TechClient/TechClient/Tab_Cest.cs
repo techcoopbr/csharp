57861f914d1346e8ad1d2f0010c277710ccba3d5
Darlan Selzlein <sdarlanset@gmail.com>
2019-02-13 01:12:06 -0200

    classe Teb_Cest

---
+++ TechClient/TechClient/Tab_Cest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechClient
+{
+    class Tab_Cest
+    {
+        public int Id { get; set; }
+        public string Cest { get; set; }
+        public string Ncm { get; set; }
+        public string Descricao { get; set; }
+    }
+}
